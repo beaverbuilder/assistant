e8ab1254fff1cb5f8f87ad1612f4483d0c837464
Justin Busa <justin@fastlinemedia.com>
2019-01-21 20:35:01 -0800

    Added Icon and ScreenHeader to public API.

--- src/front/api/index.js
+++ src/front/api/index.js
@@ -1,7 +1,13 @@
 import { registerApp } from 'apps'
+import { Icon } from 'components'
+import { ScreenHeader } from 'components/panel-parts'
 
 window.FLAssistant = {
 	apps: {
 		registerApp,
-	}
+	},
+	components: {
+		Icon,
+		ScreenHeader,
+	},
 }
