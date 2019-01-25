c94c40421bb39645491802bb038aeba31ff44d41
Brent Jett <brentjett@gmail.com>
2019-01-25 12:49:06 -0600

    Add working icons for dashboard and kitchen sink

--- src/front/apps/fl-dashboard/index.js
+++ src/front/apps/fl-dashboard/index.js
@@ -27,6 +27,13 @@ export const DashboardTab = props => {
 
 export const DashboardIcon = props => {
     return (
-        <Icon />
+        <svg width="30px" height="24px" viewBox="0 0 30 24" version="1.1" xmlns="http://www.w3.org/2000/svg">
+            <g transform="translate(-102.000000, -145.000000)" fill="transparent" fillRule="nonzero" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                <path d="M124,167.010842 C126.671358,167.010842 128.67605,167.010842 130.014075,167.010842 L130.014075,148 C130.014075,146.895431 129.118644,146 128.014075,146 L106,146 C104.895431,146 104,146.895431 104,148 L104,165.010842 C104,166.115411 104.895431,167.010842 106,167.010842 L110,167.010842"></path>
+                <path d="M130,160 C128,153.333333 123.666667,150 117,150 C110.333333,150 106,153.333333 104,160" strokeDasharray="0,5"></path>
+                <path d="M117,167 L111,157"></path>
+                <path d="M111.04454,163.319505 C110.382291,164.388826 110,165.649768 110,167 M124,167 C124,163.134007 120.865993,160 117,160"></path>
+            </g>
+        </svg>
     )
 }
--- src/front/apps/fl-kitchen-sink/index.js
+++ src/front/apps/fl-kitchen-sink/index.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 import {
     ScreenHeader,
+    ExpandedContents,
     TagGroup,
     Tag,
     Padding,
@@ -13,7 +14,8 @@ export const KitchenSinkTab = props => {
     return (
         <Fragment>
             <ScreenHeader>
-
+                These are examples of different UI components. Check out the `fl-kitchen-sink` app for details.
+                <ExpandedContents>Made Ya Look!</ExpandedContents>
             </ScreenHeader>
 
             <Padding>
@@ -48,6 +50,18 @@ export const KitchenSinkTab = props => {
 
 export const KitchenSinkIcon = props => {
     return (
-        <Icon />
+
+<svg width="29px" height="24px" viewBox="0 0 29 24" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <g stroke="currentColor" strokeWidth="2" fill="none" fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round">
+        <g transform="translate(-229.000000, -145.000000)">
+            <path d="M253,167.010842 L254.014075,167.010842 C255.118644,167.010842 256.014075,166.115411 256.014075,165.010842 L256.014075,148 C256.014075,146.895431 255.118644,146 254.014075,146 C250.792225,146 249.443621,146 248,146 M234.5,146 C234.277778,146 232.444444,146 232,146 C230.895431,146 230,146.895431 230,148 L230,165.010842 C230,166.115411 230.895431,167.010842 232,167.010842 L233,167.010842"></path>
+            <path d="M242.5,151 L242.5,154"></path>
+            <path d="M243.5,159 L243.5,160"></path>
+            <path d="M245.5,155 L245.5,157"></path>
+            <path d="M234,160 C234,164.666667 237,167 243,167 C249,167 252,164.666667 252,160"></path>
+            <polyline points="238.5 166 238.5 146 244 146 244 147.913793"></polyline>
+        </g>
+    </g>
+</svg>
     )
 }
