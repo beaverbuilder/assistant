90d6359405db92a24a01faca2b9815f37353379d
Justin Busa <justin@fastlinemedia.com>
2019-01-28 13:37:24 -0800

    Initial phpcs setup. Fixed errors.

--- classes/class-fl-assistant-asset-loader.php
+++ classes/class-fl-assistant-asset-loader.php
@@ -8,25 +8,25 @@
  */
 class FL_Assistant_Asset_Loader {
 
-    /**
-     * Initialize the backend application.
-     *
-     * @since 0.1
-     * @return void
-     */
-    static public function init() {
-        add_action( 'wp_enqueue_scripts', __CLASS__ . '::enqueue' );
-    }
+	/**
+	 * Initialize the backend application.
+	 *
+	 * @since 0.1
+	 * @return void
+	 */
+	static public function init() {
+		add_action( 'wp_enqueue_scripts', __CLASS__ . '::enqueue' );
+	}
 
-    /**
-     * Enqueue frontend resources - fired on wp_enqueue_scripts action.
-     *
-     * @since 0.1
-     * @return void
-     */
-    static public function enqueue() {
+	/**
+	 * Enqueue frontend resources - fired on wp_enqueue_scripts action.
+	 *
+	 * @since 0.1
+	 * @return void
+	 */
+	static public function enqueue() {
 
-        if ( self::should_enqueue() ) {
+		if ( self::should_enqueue() ) {
 
 			wp_enqueue_script( 'fl-assistant-front', FL_ASSISTANT_URL . 'build/front.bundle.js', array(), FL_ASSISTANT_VERSION, true );
 			wp_enqueue_style( 'fl-assistant-front', FL_ASSISTANT_URL . 'build/front.bundle.css', array(), FL_ASSISTANT_VERSION, null );
@@ -34,31 +34,31 @@ class FL_Assistant_Asset_Loader {
 			wp_localize_script( 'fl-assistant-front', 'FLAssistantInitialData', FL_Assistant_Data::get_all() );
 
 			wp_enqueue_script( 'fl-assistant-utils', FL_ASSISTANT_URL . 'build/utils.bundle.js', array(), FL_ASSISTANT_VERSION, true );
-        }
-    }
+		}
+	}
 
-    /**
-     * Check if the frontend scripts/styles should be enqueued
-     *
-     * @since 0.1
-     * @return Bool
-     */
-    static public function should_enqueue() {
+	/**
+	 * Check if the frontend scripts/styles should be enqueued
+	 *
+	 * @since 0.1
+	 * @return Bool
+	 */
+	static public function should_enqueue() {
 
-        $should_enqueue = true;
+		$should_enqueue = true;
 
-        if ( ! is_user_logged_in() || is_customize_preview() ) {
-            $should_enqueue = false;
-        }
+		if ( ! is_user_logged_in() || is_customize_preview() ) {
+			$should_enqueue = false;
+		}
 
-        // If Beaver Builder is active, don't enqueue
-        if ( class_exists( 'FLBuilderModel' ) ) {
-            if ( FLBuilderModel::is_builder_active() || FLBuilderModel::is_builder_draft_preview() ) {
-                $should_enqueue = false;
-            }
-        }
-        return $should_enqueue;
-    }
+		// If Beaver Builder is active, don't enqueue
+		if ( class_exists( 'FLBuilderModel' ) ) {
+			if ( FLBuilderModel::is_builder_active() || FLBuilderModel::is_builder_draft_preview() ) {
+				$should_enqueue = false;
+			}
+		}
+		return $should_enqueue;
+	}
 }
 
 FL_Assistant_Asset_Loader::init();
--- classes/class-fl-assistant-data.php
+++ classes/class-fl-assistant-data.php
@@ -15,132 +15,135 @@ class FL_Assistant_Data {
 	 */
 	static public $default_user_state = array(
 		'activeApp' => 'fl-dashboard',
-		'showUI' => true,
+		'showUI'    => true,
 	);
 
-    /**
-     * Returns an array of all assistant data.
+	/**
+	 * Returns an array of all assistant data.
 	 *
 	 * NOTE: Kept in alphabetical order.
-     *
-     * @since 0.1
-     * @return array
-     */
-    static public function get_all() {
+	 *
+	 * @since 0.1
+	 * @return array
+	 */
+	static public function get_all() {
 		$user_state = self::get_current_user_state();
 
-        return array(
-			'activeApp' => $user_state['activeApp'],
-			'apiNonce' => wp_create_nonce( 'wp_rest' ),
-			'apiRoot' => esc_url_raw( get_rest_url() ),
+		return array(
+			'activeApp'       => $user_state['activeApp'],
+			'apiNonce'        => wp_create_nonce( 'wp_rest' ),
+			'apiRoot'         => esc_url_raw( get_rest_url() ),
 			'currentPageView' => self::get_current_view(),
-			'contentTypes' => self::get_post_types(),
-			'currentUser' => self::get_current_user_data(),
-			'pluginURL' => FL_ASSISTANT_URL,
-			'showUI' => $user_state['showUI'],
-        );
-    }
+			'contentTypes'    => self::get_post_types(),
+			'currentUser'     => self::get_current_user_data(),
+			'pluginURL'       => FL_ASSISTANT_URL,
+			'showUI'          => $user_state['showUI'],
+		);
+	}
 
-    /**
-     * Get post type slugs and names.
-     *
-     * @since 0.1
-     * @return array
-     */
-    static public function get_post_types() {
-        $data = [];
-        $types = get_post_types( array(
-            'public' => true,
-        ), 'objects' );
+	/**
+	 * Get post type slugs and names.
+	 *
+	 * @since 0.1
+	 * @return array
+	 */
+	static public function get_post_types() {
+		$data  = [];
+		$types = get_post_types(
+			array(
+				'public' => true,
+			), 'objects'
+		);
 
-        foreach ( $types as $slug => $type ) {
+		foreach ( $types as $slug => $type ) {
 			if ( 'attachment' === $slug ) {
 				continue;
 			}
-            $data[ $slug ] = esc_html( $type->labels->name );
-        }
+			$data[ $slug ] = esc_html( $type->labels->name );
+		}
 
-        return $data;
-    }
+		return $data;
+	}
 
-    /**
-     * Get info about the current page view.
+	/**
+	 * Get info about the current page view.
 	 *
-     * @since 0.1
-     * @return array
-     */
-    static public function get_current_view() {
-        $data = [];
-        $intro = __('Currently Viewing', 'fl-assistant');
-        $name = __('Untitled', 'fl-assistant');
+	 * @since 0.1
+	 * @return array
+	 */
+	static public function get_current_view() {
+		$data  = [];
+		$intro = __( 'Currently Viewing', 'fl-assistant' );
+		$name  = __( 'Untitled', 'fl-assistant' );
 
-        if ( is_embed() ) {
+		if ( is_embed() ) {
 
-            $intro = __('Currently Viewing Embed', 'fl-assistant');
-            $name = get_the_title();
+			$intro = __( 'Currently Viewing Embed', 'fl-assistant' );
+			$name  = get_the_title();
 
-        } elseif ( is_404() ) {
+		} elseif ( is_404() ) {
 
-            $name = __('Page Not Found', 'fl-assistant');
+			$name = __( 'Page Not Found', 'fl-assistant' );
 
-        } elseif ( is_search() ) {
+		} elseif ( is_search() ) {
 
-            $intro = __('Currently Viewing Search Results For', 'fl-assistant');
-            $name = get_search_query();
+			$intro = __( 'Currently Viewing Search Results For', 'fl-assistant' );
+			$name  = get_search_query();
 
-        } elseif ( is_front_page() ) {
+		} elseif ( is_front_page() ) {
 
-            $intro = __('Currently Viewing Front Page', 'fl-assistant');
-            $name = get_the_title();
+			$intro = __( 'Currently Viewing Front Page', 'fl-assistant' );
+			$name  = get_the_title();
 
-        } elseif ( is_post_type_archive() ) {
+		} elseif ( is_post_type_archive() ) {
 
-            $intro = __('Currently Viewing Post Type Archive', 'fl-assistant');
-            $name = get_the_archive_title();
+			$intro = __( 'Currently Viewing Post Type Archive', 'fl-assistant' );
+			$name  = get_the_archive_title();
 
-        } elseif ( is_tax() ) {
+		} elseif ( is_tax() ) {
 
-            $intro = __('Currently Viewing Taxonomy', 'fl-assistant');
-            $name = get_the_title();
+			$intro = __( 'Currently Viewing Taxonomy', 'fl-assistant' );
+			$name  = get_the_title();
 
-        } elseif ( is_category() ) {
+		} elseif ( is_category() ) {
 
-            $intro = __('Currently Viewing Category', 'fl-assistant');
-            $name = get_cat_name();
+			$intro = __( 'Currently Viewing Category', 'fl-assistant' );
+			$name  = get_cat_name();
 
-        } elseif ( is_tag() ) {
+		} elseif ( is_tag() ) {
 
-            $intro = __('Currently Viewing Tag', 'fl-assistant');
-            $name = get_tag_name();
+			$intro = __( 'Currently Viewing Tag', 'fl-assistant' );
+			$name  = get_tag_name();
 
-        } elseif ( is_attachment() ) {
+		} elseif ( is_attachment() ) {
 
-            $intro = __('Currently Viewing Attachment', 'fl-assistant');
-            $name = get_the_title();
+			$intro = __( 'Currently Viewing Attachment', 'fl-assistant' );
+			$name  = get_the_title();
 
-        } elseif ( is_singular() ) {
-            $post_type = get_post_type_object(get_post_type())->labels->singular_name;
-            $intro = sprintf( esc_html__('Currently Viewing %s', 'fl-assistant'), $post_type );
-            $name = get_the_title();
-        } elseif ( is_author() ) {
-            $intro = __('Currently Viewing Author', 'fl-assistant');
-            $name = wp_get_current_user()->display_name;
-        }
+		} elseif ( is_singular() ) {
+			$post_type = get_post_type_object( get_post_type() )->labels->singular_name;
+			/* translators: post type singular name. */
+			$intro = sprintf( esc_html__( 'Currently Viewing %s', 'fl-assistant' ), $post_type );
+			$name  = get_the_title();
+		} elseif ( is_author() ) {
+			$intro = __( 'Currently Viewing Author', 'fl-assistant' );
+			$name  = wp_get_current_user()->display_name;
+		}
 
-        $data['intro'] = $intro;
-        $data['name'] = $name;
+		$data['intro'] = $intro;
+		$data['name']  = $name;
 
-        return $data;
-    }
+		return $data;
+	}
 
-    /**
-     * Get the saved state for a user.
+	/**
+	 * Get the saved state for a user.
 	 *
-     * @since 0.1
+	 * @since 0.1
 	 * @param int $id
-     * @return array
-     */
-    static public function get_user_state( $id ) {
+	 * @return array
+	 */
+	static public function get_user_state( $id ) {
 		$saved = get_user_meta( $id, 'fl_assistant_state', true );
 
 		return array_merge(
@@ -149,44 +152,46 @@ class FL_Assistant_Data {
 		);
 	}
 
-    /**
-     * Update the saved state for a user.
+	/**
+	 * Update the saved state for a user.
 	 *
-     * @since 0.1
+	 * @since 0.1
 	 * @param int $id
 	 * @param array $state
-     * @return void
-     */
-    static public function update_user_state( $id, $state ) {
+	 * @return void
+	 */
+	static public function update_user_state( $id, $state ) {
 		$saved = self::get_user_state( $id );
 
-		update_user_meta( $id, 'fl_assistant_state', array_merge(
-			$saved,
-			$state ? (array) $state : array()
-		) );
+		update_user_meta(
+			$id, 'fl_assistant_state', array_merge(
+				$saved,
+				$state ? (array) $state : array()
+			)
+		);
 	}
 
-    /**
-     * Get the saved state for the current user.
+	/**
+	 * Get the saved state for the current user.
 	 *
-     * @since 0.1
-     * @return array
-     */
-    static public function get_current_user_state() {
+	 * @since 0.1
+	 * @return array
+	 */
+	static public function get_current_user_state() {
 		return self::get_user_state( wp_get_current_user()->ID );
 	}
 
-    /**
-     * Get info about the current user.
+	/**
+	 * Get info about the current user.
 	 *
-     * @since 0.1
-     * @return array
-     */
-    static public function get_current_user_data() {
+	 * @since 0.1
+	 * @return array
+	 */
+	static public function get_current_user_data() {
 		$user = wp_get_current_user();
 
 		return array(
-			'id' => $user->ID,
+			'id'   => $user->ID,
 			'name' => $user->display_name,
 		);
 	}
