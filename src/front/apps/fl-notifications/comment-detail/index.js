931eb1bcb01af1a67f7767341c5e29ee7ef62343
Justin Busa <justin@fastlinemedia.com>
2019-02-01 16:16:44 -0800

    Added comment text to detail view.

--- src/front/apps/fl-notifications/comment-detail/index.js
+++ src/front/apps/fl-notifications/comment-detail/index.js
@@ -1,12 +1,12 @@
 import React, { Fragment } from 'react'
-import { Button, Padding } from 'components'
+import { Button, Padding, Separator } from 'components'
 import './style.scss'
 
 export const CommentDetailView = ( { data, onClose } ) => {
 	return (
 		<div className='fl-asst-comment-detail'>
 			<Padding>
-				<p>Oh hayyyyy! Viewing comment { data.id }.</p>
+				<div className="fl-asst-comment-content" dangerouslySetInnerHTML={ { __html: data.title } } />
 				<Button onClick={ onClose }>Go Back</Button>
 			</Padding>
 		</div>
