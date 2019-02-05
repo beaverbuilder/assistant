0dd3710014c69807f8e923afdfc4a6982a701e49
Justin Busa <justin@fastlinemedia.com>
2019-02-05 09:56:10 -0800

    Working on store tests.

--- src/front/store/test/actions.js
+++ src/front/store/test/actions.js
@@ -1,6 +1,7 @@
 import {
 	registerApp,
 	setActiveApp,
+	hydrateAppState,
 } from '../actions'
 
 describe( 'actions', () => {
@@ -18,6 +19,15 @@ describe( 'actions', () => {
 		it( 'should return the SET_ACTIVE_APP action', () => {
 			expect( setActiveApp( 'test' ) ).toEqual( {
 				type: 'SET_ACTIVE_APP',
+				key: 'test',
+			} )
+		} )
+	} )
+
+	describe( 'hydrateAppState', () => {
+		it( 'should return the HYDRATE_APP_STATE action', () => {
+			expect( hydrateAppState( 'test' ) ).toEqual( {
+				type: 'HYDRATE_APP_STATE',
 				key: 'test',
 			} )
 		} )
