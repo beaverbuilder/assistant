9a29a2aaf1e4712a240c5f970c90b9a68c9c06c4
Brent Jett <brentjett@gmail.com>
2019-02-12 21:39:34 -0600

    run fix

--- src/front/ui/components/modals/index.js
+++ src/front/ui/components/modals/index.js
@@ -8,23 +8,23 @@ export const useModals = () => {
 	const [ modals, setModals ] = useState( [] )
 	const [ action, setAction ] = useState()
 
-    // Render all the current modals - This gets called inside <UI />
+	// Render all the current modals - This gets called inside <UI />
 	const renderModals = () => {
 		return modals.map( ( modal ) => {
 			const { type, children, key, pose, config } = modal
 
-            if ( 'notification' === type ) {
-                return (
-                    <Notification
-                        key={key}
-                        pose={pose}
-                        onPoseComplete={onModalComplete}
-                        appearance={config.appearance}
-                    >
-                        {children}
-                    </Notification>
-                )
-            }
+			if ( 'notification' === type ) {
+				return (
+					<Notification
+						key={key}
+						pose={pose}
+						onPoseComplete={onModalComplete}
+						appearance={config.appearance}
+					>
+						{children}
+					</Notification>
+				)
+			}
 
 			return (
 				<Modal key={key} pose={pose} onPoseComplete={onModalComplete}>{children}</Modal>
@@ -34,7 +34,7 @@ export const useModals = () => {
 
 	const presentModal = ( children, config = {} ) => {
 		modals.push( {
-            type: 'modal',
+			type: 'modal',
 			key: Date.now(),
 			pose: 'offscreen',
 			children,
@@ -44,9 +44,9 @@ export const useModals = () => {
 		setAction( 'present' )
 	}
 
-    const presentNotification = ( message = '', config = { expiry: 5000, appearance: null } ) => {
-        modals.push( {
-            type: 'notification',
+	const presentNotification = ( message = '', config = { expiry: 5000, appearance: null } ) => {
+		modals.push( {
+			type: 'notification',
 			key: Date.now(),
 			pose: 'offscreen',
 			children: message,
@@ -54,7 +54,7 @@ export const useModals = () => {
 		} )
 		setModals( Array.from( modals ) )
 		setAction( 'present' )
-    }
+	}
 
 	// After DOM is mounted, "present" new model.
 	useEffect( () => {
@@ -91,7 +91,7 @@ export const useModals = () => {
 		renderModals,
 		presentModal,
 		dismissModal,
-        presentNotification,
+		presentNotification,
 	}
 }
 
@@ -143,23 +143,23 @@ const ModalBox = posed.div( {
 ModalBox.displayName = 'ModalBox'
 
 
-const Notification = ({ children, pose, onPoseComplete, appearance }) => {
-    const { dismissModal } = useContext( UIContext )
-    const classes = classname({
-        'fl-asst-notification' : true,
-        'fl-asst-notification-warning' : 'warning' === appearance,
-        'fl-asst-notification-error' : 'error' === appearance,
-    })
-    return (
-        <div className="fl-asst-modal-screen fl-asst-modal-notification-screen">
-            <NotificationBox className={classes} pose={pose} onPoseComplete={onPoseComplete}>
-                <div className="fl-asst-notification-message">{children}</div>
-                <Button onClick={dismissModal} appearance="icon">
-                    <Icon name="close" />
-                </Button>
-            </NotificationBox>
-        </div>
-    )
+const Notification = ( { children, pose, onPoseComplete, appearance } ) => {
+	const { dismissModal } = useContext( UIContext )
+	const classes = classname( {
+		'fl-asst-notification': true,
+		'fl-asst-notification-warning': 'warning' === appearance,
+		'fl-asst-notification-error': 'error' === appearance,
+	} )
+	return (
+		<div className="fl-asst-modal-screen fl-asst-modal-notification-screen">
+			<NotificationBox className={classes} pose={pose} onPoseComplete={onPoseComplete}>
+				<div className="fl-asst-notification-message">{children}</div>
+				<Button onClick={dismissModal} appearance="icon">
+					<Icon name="close" />
+				</Button>
+			</NotificationBox>
+		</div>
+	)
 }
 
 const notificationTransition = () => {
@@ -169,13 +169,13 @@ const notificationTransition = () => {
 	}
 }
 
-const NotificationBox = posed.div({
-    onscreen: {
-        y: '0%',
-        transition: notificationTransition,
-    },
-    offscreen: {
-        y: '-100%',
-        transition: notificationTransition,
-    },
-})
+const NotificationBox = posed.div( {
+	onscreen: {
+		y: '0%',
+		transition: notificationTransition,
+	},
+	offscreen: {
+		y: '-100%',
+		transition: notificationTransition,
+	},
+} )
