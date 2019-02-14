62e5e4b8fc397c9cb5eacdd182aa9f78a2e8ffc8
Justin Busa <justin@fastlinemedia.com>
2019-02-13 18:25:58 -0800

    Rework useAppState to subscribe to changes in the store.

--- src/front/store/index.js
+++ src/front/store/index.js
@@ -11,8 +11,10 @@ const store = createStore( reducers, {
 	...( 'undefined' === typeof FLAssistantInitialData ? {} : FLAssistantInitialData ),
 }, composeEnhancers( applyMiddleware( applyEffects ) ) )
 
-// Custom React Hooks
-
+/**
+ * Custom hook for subscribing local state to changes
+ * in the redux store. Returns the store's state object.
+ */
 export const useStore = () => {
 	const [ state, setState ] = useState( store.getState() )
 	const unsubscribe = store.subscribe( () => setState( store.getState() ) )
@@ -20,20 +22,27 @@ export const useStore = () => {
 	return state
 }
 
+/**
+ * Custom hook for dispatching actions to the store.
+ * Returns an object with all actions bound to dispatch.
+ */
 export const useDispatch = () => {
 	return bindActionCreators( actions, store.dispatch )
 }
 
+/**
+ * Custom hook for storing local app state values in the
+ * store. This allows these values to persist between page
+ * views as the app state is cached to local storage.
+ */
 export const useAppState = ( key, value ) => {
-	const { appState } = store.getState()
+	const { appState } = useStore()
 	const { app } = useContext( AppContext )
-	const [ state, setState ] = useState(
-		appState[ app ] && appState[ app ][ key ] ? appState[ app ][ key ] : value
-	)
-	return [ state, newState => {
-		store.dispatch( actions.setAppState( app, key, newState ) )
-		setState( newState )
-	} ]
+	const state = appState[ app ]
+	return [
+		state && undefined !== state[ key ] ? state[ key ] : value,
+		newState => store.dispatch( actions.setAppState( app, key, newState ) )
+	]
 }
 
 export default store
