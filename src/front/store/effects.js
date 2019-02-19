547a5e5806aa854b9d2280f785f6f1edf96fe182
Justin Busa <justin@fastlinemedia.com>
2019-02-19 00:10:53 -0800

    Split effects logic into before and after action. Clear app state when the active app changes.

--- src/front/store/effects.js
+++ src/front/store/effects.js
@@ -1,39 +1,56 @@
-import { hydrateAppState } from 'store/actions'
+import { hydrateAppState, clearAppState } from 'store/actions'
 import { getCache, setCache } from 'utils/cache'
 import { updateUserState } from 'utils/wordpress'
 
 export default {
-	REGISTER_APP: ( { key }, { dispatch } ) => {
-		const cache = getCache( 'app-state', key )
-		if ( cache ) {
-			dispatch( hydrateAppState( key, JSON.parse( cache ) ) )
+
+	/**
+	 * Effects that fire before an action.
+	 */
+	before: {
+		SET_ACTIVE_APP: ( action, store ) => {
+			const { activeApp } = store.getState()
+			store.dispatch( clearAppState( activeApp ) )
+		},
+	},
+
+	/**
+	 * Effects that fire after an action.
+	 */
+	after: {
+		REGISTER_APP: ( { key }, { dispatch } ) => {
+			const cache = getCache( 'app-state', key )
+			if ( cache ) {
+				dispatch( hydrateAppState( key, JSON.parse( cache ) ) )
+			}
+		},
+
+		SET_ACTIVE_APP: ( action ) => {
+			updateUserState( { activeApp: action.key } )
+		},
+
+		SET_APP_STATE: ( action, store ) => {
+			const { appState } = store.getState()
+			setCache( 'app-state', action.app, JSON.stringify( appState[ action.app ] ) )
+		},
+
+		SET_SHOW_UI: action => {
+			updateUserState( { isShowingUI: action.show } )
+		},
+
+		SET_PANEL_POSITION: ( action ) => {
+			updateUserState( { panelPosition: action.position } )
+		},
+
+		TOGGLE_PANEL_POSITION: ( action, store ) => {
+			const { panelPosition } = store.getState()
+			updateUserState( { panelPosition } )
+		},
+
+		SET_SHOULD_REDUCE_MOTION: ( action, store ) => {
+			const { shouldReduceMotion } = store.getState()
+			updateUserState( { shouldReduceMotion } )
 		}
-	},
-
-	SET_ACTIVE_APP: action => {
-		updateUserState( { activeApp: action.key } )
-	},
-
-	SET_APP_STATE: ( action, store ) => {
-		const { appState } = store.getState()
-		setCache( 'app-state', action.app, JSON.stringify( appState[ action.app ] ) )
-	},
-
-	SET_SHOW_UI: action => {
-		updateUserState( { isShowingUI: action.show } )
-	},
-
-	SET_PANEL_POSITION: ( action ) => {
-		updateUserState( { panelPosition: action.position } )
-	},
-
-	TOGGLE_PANEL_POSITION: ( action, store ) => {
-		const { panelPosition } = store.getState()
-		updateUserState( { panelPosition } )
-	},
-
-	SET_SHOULD_REDUCE_MOTION: ( action, store ) => {
-		const { shouldReduceMotion } = store.getState()
-		updateUserState( { shouldReduceMotion } )
 	}
+
 }
