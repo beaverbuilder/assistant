69b2be398456df0110f5cd2bf3655d5c1179791a
Brent Jett <brentjett@gmail.com>
2019-02-20 13:55:13 -0600

    Cleanup some more

--- src/front/ui/system/app/index.js
+++ src/front/ui/system/app/index.js
@@ -7,100 +7,100 @@ import { TunnelProvider, TunnelPlaceholder, Tunnel } from 'react-tunnels'
 import './style.scss'
 
 export const App = props => {
-    const { content } = props
-    const [isShowingAppMenu, setIsShowingAppMenu] = useState(false)
-    const showAppMenu = () => setIsShowingAppMenu( true )
-    const hideAppMenu = () => setIsShowingAppMenu( false )
-    const toggleAppMenu = () => isShowingAppMenu ? hideAppMenu() : showAppMenu()
+	const { content } = props
+	const [ isShowingAppMenu, setIsShowingAppMenu ] = useState( false )
+	const showAppMenu = () => setIsShowingAppMenu( true )
+	const hideAppMenu = () => setIsShowingAppMenu( false )
+	const toggleAppMenu = () => isShowingAppMenu ? hideAppMenu() : showAppMenu()
 
-    const context = Object.assign({}, props, {
-        isShowingAppMenu,
-        showAppMenu,
-        hideAppMenu,
-        toggleAppMenu,
-    })
-    return (
-        <AppContext.Provider value={context}>
-            <TunnelProvider>
-                <div className="fl-asst-app">
+	const context = Object.assign( {}, props, {
+		isShowingAppMenu,
+		showAppMenu,
+		hideAppMenu,
+		toggleAppMenu,
+	} )
+	return (
+		<AppContext.Provider value={context}>
+			<TunnelProvider>
+				<div className="fl-asst-app">
 
-                    <TunnelPlaceholder id="app-menu" multiple>
-                        { ({ items }) => {
-                            if ( 'undefined' !== items && items.length > 0 ) {
-                                const props = items[0]
-                                return (
-                                    <Menu {...props} />
-                                )
-                            }
-                            return null
-                        } }
-                    </TunnelPlaceholder>
+					<TunnelPlaceholder id="app-menu" multiple>
+						{ ( { items } ) => {
+							if ( 'undefined' !== items && 0 < items.length ) {
+								const props = items[0]
+								return (
+									<Menu {...props} />
+								)
+							}
+							return null
+						} }
+					</TunnelPlaceholder>
 
-                    <Stack>{ content ? content() : null }</Stack>
-                </div>
-            </TunnelProvider>
-        </AppContext.Provider>
-    )
+					<Stack>{ content ? content() : null }</Stack>
+				</div>
+			</TunnelProvider>
+		</AppContext.Provider>
+	)
 }
 
-const Menu = ({ title, children, displayBeside = 'full' }) => {
-    const { shouldReduceMotion } = useStore()
-    const { hideAppMenu, isShowingAppMenu, label } = useContext( AppContext )
-    const { appFrameSize, setAppFrameSize } = useContext( UIContext )
+const Menu = ( { title, children, displayBeside = 'full' } ) => {
+	const { shouldReduceMotion } = useStore()
+	const { hideAppMenu, isShowingAppMenu, label } = useContext( AppContext )
+	const { appFrameSize, setAppFrameSize } = useContext( UIContext )
 
-    const toggleSize = () => {
-        const sizes = ['normal', 'wide', 'full']
-        const newSize = sizes[ ( sizes.indexOf( appFrameSize ) + 1 ) % sizes.length ]
-        setAppFrameSize( newSize )
-    }
+	const toggleSize = () => {
+		const sizes = [ 'normal', 'wide', 'full' ]
+		const newSize = sizes[ ( sizes.indexOf( appFrameSize ) + 1 ) % sizes.length ]
+		setAppFrameSize( newSize )
+	}
 
-    let shouldDisplayBesideContent = false
-    if ( 'boolean' === typeof displayBeside ) {
-        shouldDisplayBesideContent = displayBeside
-    } else if ( 'full' === displayBeside && 'full' === appFrameSize ) {
-        shouldDisplayBesideContent = true
-    } else if ( 'wide' === displayBeside && ['wide', 'full'].includes( appFrameSize ) ) {
-        shouldDisplayBesideContent = true
-    }
+	let shouldDisplayBesideContent = false
+	if ( 'boolean' === typeof displayBeside ) {
+		shouldDisplayBesideContent = displayBeside
+	} else if ( 'full' === displayBeside && 'full' === appFrameSize ) {
+		shouldDisplayBesideContent = true
+	} else if ( 'wide' === displayBeside && [ 'wide', 'full' ].includes( appFrameSize ) ) {
+		shouldDisplayBesideContent = true
+	}
 
-    const classes = classname({
-        'fl-asst-app-menu' : true,
-        'fl-asst-app-menu-inline' : shouldDisplayBesideContent
-    })
+	const classes = classname( {
+		'fl-asst-app-menu': true,
+		'fl-asst-app-menu-inline': shouldDisplayBesideContent
+	} )
 
-    const overlayProps = useSpring({
-        immediate: shouldReduceMotion,
-        pointerEvents: isShowingAppMenu ? 'auto' : 'none',
-        opacity: isShowingAppMenu ? 1 : 0
-    })
-    const viewProps = useSpring({
-        width: 300,
-        transform: isShowingAppMenu || shouldDisplayBesideContent ? 'translateX(0%)' : 'translateX(-100%)',
-        immediate: shouldReduceMotion,
-    })
-    const preventClickThrough = e => e.stopPropagation()
+	const overlayProps = useSpring( {
+		immediate: shouldReduceMotion,
+		pointerEvents: isShowingAppMenu ? 'auto' : 'none',
+		opacity: isShowingAppMenu ? 1 : 0
+	} )
+	const viewProps = useSpring( {
+		width: 300,
+		transform: isShowingAppMenu || shouldDisplayBesideContent ? 'translateX(0%)' : 'translateX(-100%)',
+		immediate: shouldReduceMotion,
+	} )
+	const preventClickThrough = e => e.stopPropagation()
 
-    return (
-        <div className={classes}>
-            { !shouldDisplayBesideContent &&
+	return (
+		<div className={classes}>
+			{ ! shouldDisplayBesideContent &&
                 <animated.div className="fl-asst-app-menu-overlay" style={overlayProps} onClick={hideAppMenu} />
-            }
-            <animated.div className="fl-asst-app-menu-contents" style={viewProps} onClick={preventClickThrough}>
-                <Padding>
-                    <Heading className="fl-asst-app-menu-title">{ title ? title : label }</Heading>
-                </Padding>
-                {children}
+			}
+			<animated.div className="fl-asst-app-menu-contents" style={viewProps} onClick={preventClickThrough}>
+				<Padding>
+					<Heading className="fl-asst-app-menu-title">{ title ? title : label }</Heading>
+				</Padding>
+				{children}
 
-                <div>
-                    <Button onClick={toggleSize}>Change Size</Button>
-                </div>
-            </animated.div>
-        </div>
-    )
+				<div>
+					<Button onClick={toggleSize}>Change Size</Button>
+				</div>
+			</animated.div>
+		</div>
+	)
 }
 
 export const AppMenu = props => {
-    return (
-        <Tunnel id="app-menu" {...props} />
-    )
+	return (
+		<Tunnel id="app-menu" {...props} />
+	)
 }
