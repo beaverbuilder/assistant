0c564e33ae99b2d8686f578638bd62760f09fe0c
Brent Jett <brentjett@gmail.com>
2019-02-20 22:53:23 -0600

    really fix box shadow issue

--- src/front/ui/system/app/index.js
+++ src/front/ui/system/app/index.js
@@ -74,7 +74,8 @@ const Menu = ( { title, children, displayBeside = 'full', width = 300 } ) => {
 	} )
 	const viewProps = useSpring( {
 		width,
-		transform: isShowingAppMenu || shouldDisplayBesideContent ? 'translateX(0%)' : 'translateX(-105%)', /* hack to hide box shadow */
+		transform: isShowingAppMenu || shouldDisplayBesideContent ? 'translateX(0%)' : 'translateX(-100%)',
+		boxShadow: ! isShowingAppMenu ? 'none' : '',
 		immediate: shouldReduceMotion,
 		config: springConfig,
 	} )
