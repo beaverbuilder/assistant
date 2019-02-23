e4dd3615d359b1b6996b483b555409f1caa4d33c
Brent Jett <brentjett@gmail.com>
2019-02-23 10:49:26 -0600

    Move testing app the end

--- src/front/apps/index.js
+++ src/front/apps/index.js
@@ -1,8 +1,9 @@
+
+import './testing-frames'
+
 import './fl-settings'
 import './fl-media'
 import './fl-users'
 import './fl-find'
 import './fl-dashboard'
 import './fl-notifications'
-
-import './testing-frames'
