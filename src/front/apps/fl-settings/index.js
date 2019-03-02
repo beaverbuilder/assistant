2f98d90f8fceeda6208dc3c5c37257ab6bcf053c
Brent Jett <brentjett@gmail.com>
2019-03-02 09:33:45 -0600

    Fix preferences icon spacing

--- src/front/apps/fl-settings/index.js
+++ src/front/apps/fl-settings/index.js
@@ -33,7 +33,7 @@ const App = () => {
 
 const AppIcon = () => {
 	return (
-		<svg width="28px" height="23px" viewBox="0 0 28 23" version="1.1" xmlns="http://www.w3.org/2000/svg">
+		<svg width="29px" height="24px" viewBox="0 0 29 24" version="1.1" xmlns="http://www.w3.org/2000/svg">
 			<g stroke="currentColor" strokeWidth="2" strokeLinecap="round" fill="none" fillRule="evenodd">
 				<path d="M6,22 L3,22 C1.8954305,22 1,21.1045695 1,20 L1,3 C1,1.8954305 1.8954305,1 3,1 L3,1 L25,1 C26.1045695,1 27,1.8954305 27,3 L27,4 L27,7.5 L27,15.5 L27,19 L27,20 C27,21.1045695 26.1045695,22 25,22 L11,22" />
 				<path d="M1,7.5 L7,7.5 M12,7.5 L27,7.5" />
