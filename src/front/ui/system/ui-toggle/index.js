1a25f5e70bf3ea2c5a9e70fefd9fb207532437f5
Brent Jett <brentjett@gmail.com>
2019-03-04 11:36:01 -0600

    Clean up

--- src/front/ui/system/ui-toggle/index.js
+++ src/front/ui/system/ui-toggle/index.js
@@ -2,28 +2,26 @@ import React, { useContext } from 'react'
 import { Button, Branding, UIContext } from 'components'
 import { animated, useSpring } from 'react-spring'
 import './style.scss'
-/**
- * Button To Show/Hide The UI
- */
+
 export const UIToggleButton = () => {
 	const { isShowingUI, setIsShowingUI, panelPosition } = useContext( UIContext )
 
-    const divProps = useSpring({
-        transform: isShowingUI ? 'scale(0)' : 'scale(1)'
-    })
+	const divProps = useSpring( {
+		transform: isShowingUI ? 'scale(0)' : 'scale(1)'
+	} )
 
 	const styles = {
 		position: 'fixed',
 		right: 'end' === panelPosition ? 0 : 'auto',
-        left: 'start' === panelPosition ? 0 : 'auto',
+		left: 'start' === panelPosition ? 0 : 'auto',
 		bottom: 0,
 		padding: 15,
 		zIndex: 999,
-        transformOrigin: 'start' === panelPosition ? 'bottom left' : 'bottom right',
-        ...divProps,
+		transformOrigin: 'start' === panelPosition ? 'bottom left' : 'bottom right',
+		...divProps,
 	}
 	const buttonStyles = {
-        margin:0,
+		margin: 0,
 		borderRadius: '8px',
 		border: 'none',
 	}
