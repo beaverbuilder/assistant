cc2990a4eef6b374b07426a63b47b28d6e079c8c
Brent Jett <brentjett@gmail.com>
2019-03-11 16:54:09 -0500

    Add readme.tx

--- fl-assistant.php
+++ fl-assistant.php
@@ -2,10 +2,13 @@
 /*
  * Plugin Name: Assistant
  * Plugin URI: TBD
- * Author: Justin, Robby & Brent
- * Version: 0.1
- * Description: A tool for seeing what's happening inside WordPress and quickly navigating between pages & posts.
+ * Author: The Beaver Builder Team
+ * Version: 0.1.0
+ * Description: A tool for navigating a site and accomplishing quick tasks without needing the WordPress admin.
+ * License: GNU General Public License v2.0
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: fl-assistant
+ * Copyright: (c) 2019 Beaver Builder
 */
 
 defined( 'ABSPATH' ) || die();
