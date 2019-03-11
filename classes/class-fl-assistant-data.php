31135413abe6118ddb94522076ac78d117888d78
Brent Jett <brentjett@gmail.com>
2019-03-11 16:57:16 -0500

    default panel to showing

--- classes/class-fl-assistant-data.php
+++ classes/class-fl-assistant-data.php
@@ -10,7 +10,7 @@ class FL_Assistant_Data {
 	 */
 	static public $default_user_state = array(
 		'activeApp'          => 'fl-dashboard',
-		'isShowingUI'        => false,
+		'isShowingUI'        => true,
 		'panelPosition'      => 'end',
 		'shouldReduceMotion' => false,
 		'appFrameSize'       => 'normal',
