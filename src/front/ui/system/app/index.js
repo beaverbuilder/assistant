552e2505d6db6ebcf49adff0986117b4084181d5
Brent Jett <brentjett@gmail.com>
2019-03-18 18:03:25 -0500

    intercept click events on app content when header is expanded

--- src/front/ui/system/app/index.js
+++ src/front/ui/system/app/index.js
@@ -209,10 +209,11 @@ const AppContentWrapper = ( { children } ) => {
 	return (
 		<div
 			className={classes}
-			onClick={ () => {
+			onClickCapture={ e => {
 				if ( isAppHeaderExpanded ) {
 					setIsAppHeaderExpanded( false )
 				}
+				e.stopPropagation()
 			}}
 		>{children}</div>
 	)
