8c461052da2e5c31e13ac524061ed71fe5b147c8
Fred Isaacs <fred.isaacs@gmail.com>
2019-07-14 14:02:39 -0700

    fix for #147

---
+++ tests/Rest/UpdatesControllerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace FL\Assistant\Tests\Rest;
+
+
+class UpdatesControllerTest extends RestTestCase {
+
+	public function test_can_list_updates() {
+		wp_set_current_user( 1 );
+
+		$namespace = '/fl-assistant/v1';
+		$request   = new \WP_REST_Request( 'GET', $namespace . '/updates' );
+		$response  = $this->server->dispatch( $request );
+
+		$this->assertEquals( 200, $response->get_status() );
+		$data = $response->get_data();
+
+		$this->assertIsArray($data);
+	}
+
+	public function test_can_get_updates_count() {
+		wp_set_current_user( 1 );
+
+		$namespace = '/fl-assistant/v1';
+		$request   = new \WP_REST_Request( 'GET', $namespace . '/updates/count' );
+		$response  = $this->server->dispatch( $request );
+
+		$this->assertEquals( 200, $response->get_status() );
+
+		$data = $response->get_data();
+
+		$this->assertArrayHasKey('plugins', $data);
+		$this->assertArrayHasKey('themes', $data);
+		$this->assertArrayHasKey('total', $data);
+	}
+}
