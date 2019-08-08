be9b998993fb64003e0b7f4a4b407cd3663e847c
Brent Jett <brentjett@gmail.com>
2019-08-08 11:32:24 -0500

    Improve window drag performance. Add drop indicators when dragging window.

--- src/ui/main/index.js
+++ src/ui/main/index.js
@@ -58,10 +58,6 @@ const WindowToolbar = () => {
 	const { label } = useContext( App.Context )
 
 	const stopProp = e => e.stopPropagation()
-	const stopEvts = {
-		onMouseMove: stopProp,
-		onTouchStart: stopProp,
-	}
 
 	const labelStyle = { padding: '2px 10px' }
 	const iconWrapStyle = { display: 'inline-flex', transform: 'translateY(2px)' }
