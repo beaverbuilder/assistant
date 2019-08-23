a35527f686d1632c9e79be12522434c1092b7f1c
Brent Jett <brentjett@gmail.com>
2019-08-22 21:05:04 -0500

    Run fix

--- src/ui/admin-bar-item/index.js
+++ src/ui/admin-bar-item/index.js
@@ -5,15 +5,15 @@ const { window } = store.getState()
 
 if ( 'admin_bar' === window.hiddenAppearance ) {
 
-    document.addEventListener( 'DOMContentLoaded', () => {
+	document.addEventListener( 'DOMContentLoaded', () => {
 
-        const el = document.querySelector('#wp-admin-bar-fl_assistant_toggle_ui')
-        const { toggleIsShowingUI } = getSystemActions()
+		const el = document.querySelector( '#wp-admin-bar-fl_assistant_toggle_ui' )
+		const { toggleIsShowingUI } = getSystemActions()
 
-        if ( el ) {
-            el.addEventListener( 'click', () => {
-                toggleIsShowingUI()
-            } )
-        }
-    })
+		if ( el ) {
+			el.addEventListener( 'click', () => {
+				toggleIsShowingUI()
+			} )
+		}
+	} )
 }
--- src/ui/main/index.js
+++ src/ui/main/index.js
@@ -47,7 +47,7 @@ const MainWindow = () => {
 		}
 		*/
 
-	}, [])
+	}, [] )
 
 	return (
 		<Window
