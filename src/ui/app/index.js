d536207bc7a799ff42214beffe47270fa35d083b
Brent Jett <brentjett@gmail.com>
2019-08-28 10:57:55 -0500

    Remove CSSTransitionGroup for now.

--- src/ui/app/index.js
+++ src/ui/app/index.js
@@ -1,6 +1,5 @@
 import React, { forwardRef } from 'fl-react'
 import { withRouter } from 'fl-react-router-dom'
-import { TransitionGroup, CSSTransition } from 'react-transition-group'
 import classname from 'classnames'
 import { __ } from 'assistant/i18n'
 import { Page, Nav, Error } from 'assistant/ui'
@@ -8,26 +7,13 @@ import { useSystemState } from 'assistant/data'
 import { HomeScreen } from './home-screen'
 import './style.scss'
 
-export const AppRouting = withRouter(  ( { location, history } ) => {
-
-	const shouldTransitionCard = false
-	const classes = classname( {
-		[ history.action ]: shouldTransitionCard
-	} )
+export const AppRouting = withRouter(  ( { location } ) => {
 	return (
-		<TransitionGroup className="fl-asst-transition-group">
-			<CSSTransition
-				key={ location.key }
-				classNames={ classes }
-				timeout={ shouldTransitionCard ? 210 : 0 }
-			>
-				<Nav.Switch location={ location }>
-					<Nav.Route exact path="/" component={ HomeScreen } />
-					<Nav.Route path="/:app" component={ AppContent } />
-					<Nav.Route component={ NoApp } />
-				</Nav.Switch>
-			</CSSTransition>
-		</TransitionGroup>
+		<Nav.Switch location={ location }>
+			<Nav.Route exact path="/" component={ HomeScreen } />
+			<Nav.Route path="/:app" component={ AppContent } />
+			<Nav.Route component={ NoApp } />
+		</Nav.Switch>
 	)
 } )
 
