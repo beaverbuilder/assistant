3eab5d188cd9672d5b350c231cb42eab76302c32
Justin Busa <justin@fastlinemedia.com>
2019-09-10 18:49:33 -0700

    Fixed list race events. Added new useListItems hook.

--- src/system/lib/lists/wordpress/index.js
+++ src/system/lib/lists/wordpress/index.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'fl-react'
-import uuidv1 from 'uuid/v1'
+import React, { useEffect } from 'fl-react'
 import { CancelToken, isCancel } from 'axios'
 import { getWpRest } from 'shared-utils/wordpress'
 import { List } from 'lib'
@@ -12,64 +11,21 @@ export const WordPress = ( {
 	query = {},
 	...rest,
 } ) => {
-	const [ items, setItems ] = useState( [] )
+	const { items, setItems, updateItem, removeItem, cloneItem } = List.useListItems()
 	const { getPagedContent } = getWpRest()
 	const source = CancelToken.source()
-	const offset = items.length
 
 	useEffect( () => {
 		setItems( [] )
 	}, [ type, JSON.stringify( query ) ] )
 
-	const getItemKey = ( uuid ) => {
-		for ( let i = 0; i < items.length; i++ ) {
-			if ( 'undefined' === typeof items[ i ].uuid ) {
-				continue
-			} else if ( items[ i ].uuid == uuid ) {
-				return i
-			}
-		}
-		return -1
-	}
-
-	const updateItem = ( uuid, newProps = {} ) => {
-		const key = getItemKey( uuid )
-		if ( key > -1 ) {
-			items[ key ] = Object.assign( items[ key ], newProps )
-			setItems( [ ...items ] )
-		}
-	}
-
-	const removeItem = ( uuid ) => {
-		const key = getItemKey( uuid )
-		if ( key > -1 ) {
-			items.splice( key, 1 )
-			setItems( [ ...items ] )
-		}
-	}
-
-	const cloneItem = ( uuid, newProps = {} ) => {
-		const key = getItemKey( uuid )
-		if ( key > -1 ) {
-			const clone = Object.assign( {}, items[ key ], newProps )
-			clone.uuid = uuidv1()
-			items.splice( key + 1, 0, clone )
-			setItems( [ ...items ] )
-			return clone
-		}
-		return null
-	}
-
 	return (
 		<List.Scroller
 			items={ formatItems( items ) }
 			loadItems={ ( setHasMore ) => {
-				getPagedContent( type, query, offset, {
+				getPagedContent( type, query, items.length, {
 					cancelToken: source.token,
 				} ).then( response  => {
-					response.data.items.map( ( item, i ) => {
-						response.data.items[ i ].uuid = uuidv1()
-					} )
 					setItems( items.concat( response.data.items ) )
 					setHasMore( response.data.has_more )
 					onItemsLoaded( response )
@@ -85,7 +41,6 @@ export const WordPress = ( {
 					removeItem,
 					cloneItem,
 					updateItem,
-					getItemKey,
 				} )
 			} }
 			{ ...rest }
