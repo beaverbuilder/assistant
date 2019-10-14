10cce52fb06a6efd5bc2925f7ee463c284aeab23
Justin Busa <justin@fastlinemedia.com>
2019-10-14 11:36:45 -0700

    Added logic for installing default labels.

--- backend/src/PostTypes/NotationsPostType.php
+++ backend/src/PostTypes/NotationsPostType.php
@@ -3,12 +3,19 @@
 namespace FL\Assistant\PostTypes;
 
 use FL\Assistant\System\Contracts\PostTypeAbstract;
+use FL\Assistant\Data\Repository\NotationsRepository;
 
 /**
  * Class NotationsPostType
  * @package FL\Assistant\PostTypes
  */
 class NotationsPostType extends PostTypeAbstract {
+
+	protected $notations;
+
+	public function __construct( NotationsRepository $notations ) {
+		$this->notations = $notations;
+	}
 
 	/**
 	 * @return void
@@ -17,15 +24,17 @@ class NotationsPostType extends PostTypeAbstract {
 		register_post_type(
 			'fl_asst_notation', [
 				'label'  => __( 'Assistant Notation', 'fl-assistant' ),
-				'public' => false,
+				'public' => true,
 			]
 		);
 
 		register_taxonomy( 'fl_asst_label', [ 'fl_asst_notation' ], [
 			'label'             => _x( 'Label', 'Custom taxonomy label.', 'fl-assistant' ),
 			'hierarchical'      => false,
-			'public'            => false,
+			'public'            => true,
 			'show_admin_column' => false,
 		] );
+
+		$this->notations->save_default_labels();
 	}
 }
