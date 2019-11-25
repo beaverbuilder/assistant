bc0f0bf79fa4f8bc6e0497dad947a22e9123483a
Brent Jett <brentjett@gmail.com>
2019-11-25 13:21:22 -0600

    move config import to after api setup

--- src/system/index.js
+++ src/system/index.js
@@ -1,11 +1,6 @@
 import { api as data } from 'store'
 import { api as ui } from 'lib'
 import utils from './utils'
-import * as i18n from '@wordpress/i18n'
-import * as cloud from 'utils/cloud'
-
-// After everything is imported
-import './config'
 
 const api = window.FL || {}
 const asst = FL.Assistant || {}
@@ -21,10 +16,12 @@ const Assistant = {
 	data,
 	ui,
 	utils,
-	cloud
 }
 
 window.FL = {
 	...api,
 	Assistant,
 }
+
+// After everything is set up
+import './config'
