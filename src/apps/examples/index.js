38681cc134ddcd48a02fbbc808ecee28a50b86fc
Brent Jett <brentjett@gmail.com>
2019-12-10 20:29:32 -0600

    Make Examples app only register in dev mode.

--- src/apps/examples/index.js
+++ src/apps/examples/index.js
@@ -2,10 +2,11 @@ import { registerApp } from 'assistant'
 import { __ } from '@wordpress/i18n'
 import { App } from './app'
 
-registerApp( 'fl-ui-examples', {
-	label: __( 'Examples' ),
-	root: App,
-	icon: App.Icon,
-	accent: { color: '#8bc34a' },
-	isEnabled: ! __PRODUCTION__,
-} )
+if ( !__PRODUCTION__ ) {
+	registerApp( 'fl-ui-examples', {
+		label: __( 'UI Examples' ),
+		root: App,
+		icon: App.Icon,
+		accent: { color: '#8bc34a' },
+	} )
+}
