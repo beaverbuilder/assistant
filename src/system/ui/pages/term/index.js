5ca58cf559f19dee331083bd60dbe66567be2b84
shahid <shahid.ajmeri@multidots.com>
2020-01-08 18:38:27 +0530

    added terms edit functionality

--- src/system/ui/pages/term/index.js
+++ src/system/ui/pages/term/index.js
@@ -1,40 +1,97 @@
 import React from 'react'
 import { __ } from '@wordpress/i18n'
-import { Page, Form } from 'ui'
+import { Page, Form, Button } from 'ui'
+import { getWpRest } from 'utils/wordpress'
+import { getSystemActions } from 'data'
 
-export const Term = ( { location } ) => {
+export const Term = ({ location }) => {
 	const { item } = location.state
-	const { form, useFormContext } = Form.useFormData( {
+	const wpRest = getWpRest()
+	const { setCurrentHistoryState } = getSystemActions()
+	const onSubmit = ({ changed, ids, setValue }) => {
+
+		const data = {
+			meta: {},
+		}
+		for (let key in changed) {
+			if (!ids[key]) {
+				continue
+			}
+			data[ids[key]] = changed[key]
+		}
+
+		if ('description' in changed) {
+			item.description = changed.description
+		}
+
+		if ('title' in changed) {
+			item.title = changed.title
+			data.name = changed.title
+		}
+
+		if ('slug' in changed) {
+			item.slug = changed.slug
+		}
+
+		wpRest.terms().update(item.id, 'data', data).then(response => {
+			const { data } = response
+			if (data.error) {
+				handleError()
+			} else {
+				setCurrentHistoryState({ item })
+				setIsSubmitting(false)
+				alert(__('Changes Saved!'))
+			}
+		}).catch(error => {
+			handleError(error)
+		})
+	}
+
+	const { form, useFormContext, hasChanges, resetForm, submitForm, setIsSubmitting } = Form.useFormData({
 		title: {
-			label: __( 'Name' ),
+			label: __('Name'),
 			labelPlacement: 'beside',
+			component: 'text',
 		},
 		slug: {
-			label: __( 'Slug' ),
+			label: __('Slug'),
 			labelPlacement: 'beside',
+			component: 'text',
 		},
 		parent: {
-			label: __( 'Parent' ),
+			label: __('Parent'),
 			labelPlacement: 'beside',
 			options: {
-				'': __( 'None' ),
+				'': __('None'),
 			},
 		},
 		description: {
-			label: __( 'Description' ),
-			type: 'textarea',
-			rows: 4,
+			label: __('Description'),
+			labelPlacement: 'beside',
+			component: 'textarea',
+			rows: 6,
 		},
-	}, {}, item )
+	}, { onSubmit }, item)
 
+	const Footer = () => {
+		return (
+			<>
+				<Button
+					onClick={resetForm}
+				>{__('Cancel')}</Button>
+				<div style={{ flex: '1 1 auto', margin: 'auto' }} />
+				<Button type="submit" status="primary" onClick={submitForm}>{__('Save')}</Button>
+			</>
+		)
+	}
 	return (
-		<Page.NewPage title={ __( 'Edit Term' ) } padX={ false }>
-			<Form { ...form }>
+		<Page.NewPage title={__('Edit Term')} padX={true} footer={hasChanges && <Footer />}>
+			<Form {...form}>
 				<Page.RegisteredSections
-					location={ { type: 'term' } }
-					data={ { useFormData: useFormContext } }
+					location={{ type: 'term' }}
+					data={{ useFormData: useFormContext }}
 				/>
 			</Form>
-		</Page.NewPage>
+		</Page.NewPage >
 	)
 }
