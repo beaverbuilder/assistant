06bd0a1a2727e7843f755a128e648c238954c9fe
Justin Busa <justin@fastlinemedia.com>
2020-01-14 13:47:39 -0800

    Reworked create post for refactored form data hook.

--- src/system/ui/pages/post/parent.js
+++ src/system/ui/pages/post/parent.js
@@ -4,25 +4,28 @@ import { getWpRest } from 'utils/wordpress'
 
 export const useParentOptions = ( type ) => {
 	const wpRest = getWpRest()
-	const [ options, setOptions ] = useState( {
-		0: __( 'None' ),
-	} )
+	const [ options, setOptions ] = useState( {} )
 
 	useEffect( () => {
+		setOptions( {} )
 		wpRest.posts().hierarchical( {
 			hide_empty: 0,
 			post_type: type,
 			posts_per_page: -1,
 		} ).then( response => {
 			if ( undefined !== response.data && Array.isArray( response.data ) ) {
+				const options = {}
 				response.data.map( post => {
 					options[ 'parent:' + post.id ] = post.title
 					setParentChildOptions( options, post.children )
 				} )
-				setOptions( { ...options } )
+				setOptions( {
+					0: __( 'None' ),
+					...options
+				} )
 			}
 		} )
-	}, [] )
+	}, [ type ] )
 
 	return options
 }
