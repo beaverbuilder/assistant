2760954bf6dad104ff241e6086abd74c8408f479
shahid <shahid.ajmeri@multidots.com>
2020-01-18 18:17:51 +0530

    Refactored the detail view of the media app using the Form.useForm hook. Hooked up all form fields to the backend. Added file size, upload date, and type added delete action

--- src/system/ui/pages/attachment/index.js
+++ src/system/ui/pages/attachment/index.js
@@ -1,97 +1,198 @@
 import React from 'react'
 import { __ } from '@wordpress/i18n'
-import { Page, Form } from 'ui'
+import { Page, Form, Button } from 'ui'
 import { getSrcSet } from 'utils/image'
+import { getWpRest } from 'utils/wordpress'
+import { getSystemActions } from 'data'
 
-const getItemActions = ( { staticValues } ) => {
-
-	const { url, editUrl } = staticValues
-
-	return [
-		{
-			label: __( 'View Attachment' ),
-			href: url,
-		},
-		{
-			label: __( 'Edit in Admin' ),
-			href: editUrl,
-		},
-	]
-}
-
-export const Attachment = ( { location } ) => {
+export const Attachment = ( { location, history } ) => {
 	const { item } = location.state
 	const srcSet = getSrcSet( item.sizes )
+	const wpRest = getWpRest()
+	const { setCurrentHistoryState } = getSystemActions()
+	const {
+		id,
+		title,
+		description,
+		type,
+		subtype,
+		filesize,
+		date
+	} = item
 
-	// Form Handler
-	const { form, useFormContext } = Form.useFormData( {
-		title: {
-			label: __( 'Title' ),
-			labelPlacement: 'beside',
+
+	const onSubmit = ( { changed, ids } ) => {
+
+		const data = {
+			meta: {},
+		}
+		for ( let key in changed ) {
+			if ( ! ids[key] ) {
+				continue
+			}
+			data[ids[key]] = changed[key]
+		}
+
+
+
+		if ( 'title' in changed ) {
+			item.title = changed.title
+		}
+
+		if ( 'alt' in changed ) {
+			item.alt = changed.alt
+		}
+
+		if ( 'description' in changed ) {
+			item.description = changed.description
+		}
+
+		const handleError = error => {
+			setIsSubmitting( false )
+			alert( __( 'Error: Changes not saved! Please try again.' ) )
+			if ( error ) {
+				console.log( error ) // eslint-disable-line no-console
+			}
+		}
+
+		wpRest.attachments().update( id, 'data', data ).then( response => {
+			const { data } = response
+			console.log(data.error)
+			if ( data.error ) {
+				handleError()
+			} else {
+				setCurrentHistoryState( { item } )
+				setIsSubmitting( false )
+				alert( __( 'Changes Saved!' ) )
+			}
+		} ).catch( error => {
+			handleError( error )
+		} )
+
+	}
+
+	const deleteAttchment = () => {
+		if ( confirm( __( 'Do you really want to delete this media?' ) ) ) {
+			wpRest.attachments().update( id, 'trash' ).then( () => {
+				alert( 'Media permanently deleted!' )
+			} )
+			history.goBack()
+		}
+	}
+
+	const sections = {
+		labels: {
+			label: __( 'Labels' ),
+			fields: {
+				labels: {
+					component: 'labels',
+					alwaysCommit: true,
+					onAdd: label => {
+						wpRest.notations().createLabel( 'post', item.id, label.id )
+					},
+					onRemove: label => {
+						wpRest.notations().deleteLabel( 'post', item.id, label.id )
+					},
+				},
+			}
 		},
-		alt: {
-			label: __( 'Alternative Text' ),
-			labelPlacement: 'beside',
+		meta: {
+			label: __( 'Metadata' ),
+			fields: {
+				title: {
+					label: __( 'Title' ),
+				},
+				alt: {
+					label: __( 'Alternative Text' ),
+				},
+				description: {
+					label: __( 'Description' ),
+					component: 'textarea',
+					rows: 2,
+				},
+				filesize: {
+					label: __( 'File size' ),
+					component: 'plain-text',
+				},
+				type: {
+					label: __( 'File type' ),
+					component: 'plain-text',
+				},
+				date: {
+					label: __( 'Uploaded on' ),
+					component: 'plain-text',
+				},
+			},
 		},
-		description: {
-			type: 'textarea',
-			label: __( 'Description' ),
-			rows: 2,
-		},
-		caption: {
-			type: 'textarea',
-			label: __( 'Caption' ),
-			rows: 2,
-		},
-		url: {
-			label: __( 'URL' ),
+		links: {
+			label: __( 'Links' ),
+			fields: {
+				fileUrl: {
+					label: __( 'File URL' ),
+					component: 'url',
+					isVisible: !! item.sizes.full,
+				},
+				url: {
+					label: __( 'URL' ),
+					component: 'url',
+				},
+			},
 		},
 		actions: {
-			value: getItemActions,
-		},
-	},
-	{ /* options */ }, item )
+			label: __( 'Actions' ),
+			fields: {
+				actions: {
+					component: 'actions',
+					options: [
+						{
+							label: __( 'View Attachment' ),
+							href: item.url,
+						},
+						{
+							label: __( 'Edit in Admin' ),
+							href: item.editUrl,
+						},
+						{
+							label: __( 'Delete' ),
+							status:'destructive',
+							onClick:deleteAttchment
 
-	const sectionData = {
-		attachment: item,
-
-		useFormData: useFormContext,
-
-		actions: [
-			{
-				label: __( 'View Attachment Page' ),
-				href: '#'
-			},
-			{
-				label: __( 'Edit in Admin' ),
-				href: '#'
-			},
-			{
-				label: __( 'Replace File' ),
-				onClick: () => {}
-			},
-			{
-				label: __( 'Refresh Thumbnails' ),
-				onClick: () => {}
-			},
-			{
-				label: __( 'Move to Trash' ),
-				onClick: () => {}
-			},
-		],
+						},
+					]
+				}
+			}
+		}
 	}
+
+	const Footer = () => {
+		return (
+			<>
+				<Button
+					onClick={ resetForm }
+				>{__( 'Cancel' )}</Button>
+				<div style={ { flex: '1 1 auto', margin: 'auto' } } />
+				<Button type="submit" status="primary" onClick={ submitForm }>{__( 'Save' )}</Button>
+			</>
+		)
+	}
+
+	const defaults = {
+		...item,
+		fileUrl: item.sizes.full ? item.sizes.full.url : null,
+		type: type+'/'+subtype
+	}
+
+	const { hasChanges, resetForm, setIsSubmitting, submitForm, renderForm } = Form.useForm( {
+		sections,
+		onSubmit,
+		defaults,
+	} )
 
 	const Hero = () => <img src={ item.thumbnail } srcSet={ srcSet } />
 
 	return (
-		<Page title={ __( 'Attachment' ) } hero={ <Hero /> }>
-
-			<Form { ...form }>
-				<Page.RegisteredSections
-					location={ { type: 'attachment' } }
-					data={ sectionData }
-				/>
-			</Form>
+		<Page title={ __( 'Attachment' ) } hero={ <Hero /> } footer={ hasChanges && <Footer /> }>
+			{ renderForm() }
 		</Page>
 	)
 }
