a4567403e5116cf43f6b59284f7ff9b6499dcff8
Brent Jett <brentjett@gmail.com>
2020-02-12 15:07:54 -0600

    Add icon for labels app

--- src/apps/fl-labels/index.js
+++ src/apps/fl-labels/index.js
@@ -1,8 +1,9 @@
 import { registerApp } from 'assistant'
 import { __ } from '@wordpress/i18n'
-import { App } from './app'
+import { App, Icon } from './app'
 
 registerApp( 'fl-labels', {
 	label: __( 'Labels' ),
 	root: App,
+	icon: App.Icon,
 } )
