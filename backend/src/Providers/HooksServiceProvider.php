2fc370daa17cd1a8a44548480c7dd6b1b7e7e744
Brent Jett <brentjett@gmail.com>
2020-02-17 15:39:11 -0600

    Implement labels in admin post lists.

--- backend/src/Providers/HooksServiceProvider.php
+++ backend/src/Providers/HooksServiceProvider.php
@@ -13,6 +13,10 @@ use FL\Assistant\Hooks\Actions\OnDeleteTerm;
 use FL\Assistant\Hooks\Filters\OnHeartbeatReceived;
 use FL\Assistant\Hooks\Filters\OnFLBuilderUIBarButtons;
 use FL\Assistant\System\Contracts\ServiceProviderAbstract;
+use FL\Assistant\Data\Transformers\NotationsTransformer;
+use FL\Assistant\Data\Repository\NotationsRepository;
+use FL\Assistant\Data\Repository\LabelsRepository;
+use FL\Assistant\Data\Transformers\LabelsTransformer;
 
 class HooksServiceProvider extends ServiceProviderAbstract {
 
@@ -59,5 +63,54 @@ class HooksServiceProvider extends ServiceProviderAbstract {
 		add_filter( 'heartbeat_received', $this->injector->make( OnHeartbeatReceived::class ), 11, 2 );
 
 		add_filter( 'fl_builder_ui_bar_buttons', $this->injector->make( OnFLBuilderUIBarButtons::class ) );
+
+		// Setup custom WP admin post list columns
+		$this->enqueue_custom_admin_columns();
+	}
+
+	public function enqueue_custom_admin_columns() {
+
+		$types = get_post_types( [ 'public' => true, ], 'objects' );
+
+		foreach( $types as $type => $info ) {
+			add_filter( "manage_{$type}_posts_columns", [$this, 'add_columns'] );
+			add_action( "manage_{$type}_posts_custom_column", [ $this, 'render_column' ], 10, 2 );
+		}
+	}
+
+	public function add_columns( $columns ) {
+		$columns['fl_assistant'] = __( 'Assistant', 'fl-assistant' );
+		return $columns;
+	}
+
+	public function render_column( $column, $post_id ) {
+		$vars = "--fl-asst-red: #FF5335; --fl-asst-blue: #1BADF8; --fl-asst-green: #00D281; --fl-asst-yellow: #FFD000; --fl-asst-orange: #FF9500; --fl-asst-purple: #CC73E1; --fl-asst-pink: #FF2968;";
+		$styles = "{$vars} padding: 2px 5px; display: inline-flex; flex: 0 0 auto; border-radius: 20px; border: 1px solid #d2d2d2; align-items: center; color: black; background: white; margin: 2px 5px 3px; margin-left:0;";
+		$dot = "width: 10px; height: 10px; flex: 0 0 10px; border-radius: 7px; background: blue; margin-right: 5px;";
+
+		if ( 'fl_assistant' === $column ) {
+			$transformer = new NotationsTransformer;
+			$repository = new NotationsRepository( $transformer );
+			$labels = $repository->get_labels( 'post', $post_id );
+
+			$labels_repo = new LabelsRepository;
+			$labels_transformer = new LabelsTransformer( $labels_repo );
+			$terms = $labels_repo->query( [ 'hide_empty' => false ] )->get_terms();
+
+			// Assemble term meta dataset
+			$term_meta = [];
+			foreach( $terms as $key => $term ) {
+				$item = call_user_func( $labels_transformer, $term );
+				$term_meta[ $item['id'] ] = $item;
+			}
+
+			// Output labels
+			foreach( $labels as $label ) {
+				$term = $term_meta[ $label['label_id'] ];
+				if ( isset( $term ) ) {
+					print "<span style='{$styles}'><span style='{$dot}; background-color: {$term['color']}'></span>{$term['label']}</span>";
+				}
+			}
+		}
 	}
 }
