a7953d374910f378409f8ea6cb65e3b48259caed
Brent Jett <brentjett@gmail.com>
2020-03-24 12:40:57 -0500

    fix up and down caret icons getting filled by TwentyNineteen theme

--- src/system/ui/art/icon/index.js
+++ src/system/ui/art/icon/index.js
@@ -264,7 +264,7 @@ Icon.Home = () => {
 Icon.DownCaret = () => {
 	return (
 		<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-			<path d="M16 7L10 13L4 7" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+			<path d="M16 7L10 13L4 7" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" fill="none" />
 		</svg>
 	)
 }
@@ -272,7 +272,7 @@ Icon.DownCaret = () => {
 Icon.UpCaret = () => {
 	return (
 		<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-			<path d="M16 13L10 7L4 13" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+			<path d="M16 13L10 7L4 13" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" fill="none" />
 		</svg>
 	)
 }
