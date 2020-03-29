a66dc065786591e8c8e0b66d021b9d160d5be5c4
Justin Busa <justin@fastlinemedia.com>
2020-03-28 20:17:12 -0700

    Added uncommitted files.

---
+++ src/apps/fl-cloud/pages/auth/layout.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { __ } from '@wordpress/i18n'
+import classname from 'classnames'
+import { Page } from 'assistant/ui'
+import AppIcon from '../../icon'
+import './style.scss'
+
+export default ( { className, children } ) => {
+	const classes = classname( className, {
+		'fl-asst-auth-layout': true,
+	} )
+	return (
+		<Page
+			className={ classes }
+			title={ __( 'Cloud' ) }
+			icon={ <AppIcon context="sidebar" /> }
+			shouldShowBackButton={ false }
+		>
+			{ children }
+		</Page>
+	)
+}
