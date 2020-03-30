bbe15ac9f0b4ca780f767dc7482f3723e00a6fcb
Brent Jett <brentjett@gmail.com>
2020-03-29 21:06:13 -0500

    remove `withWarning` HoC. We weren't using it.

--- packages/@beaverbuilder/fluid/src/ui/button/index.js
+++ packages/@beaverbuilder/fluid/src/ui/button/index.js
@@ -1,10 +1,7 @@
 import TooltipButton from './tooltip-button'
-import { withWarning } from '../../utils'
 import './style.scss'
 
 const Button = TooltipButton
 Button.displayName = 'Button'
-
-Button.Test = withWarning(Button, 'there is a problem here')
 
 export default Button
