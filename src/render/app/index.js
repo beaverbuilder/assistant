e0679b7cc3e4253e1783359e8232c058dcc46e16
Brent Jett <brentjett@gmail.com>
2020-03-31 10:35:00 -0500

    Remove unused NavToolbar

--- src/render/app/index.js
+++ src/render/app/index.js
@@ -1,8 +1,8 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import classname from 'classnames'
 import { useLocation } from 'react-router-dom'
 import { __ } from '@wordpress/i18n'
-import { App, Nav, Page, Icon, Button, Env } from 'assistant/ui'
+import { App, Nav, Page, Env } from 'assistant/ui'
 import { useSystemState } from 'assistant/data'
 import HomeScreen from './home-screen'
 import Sidebar from './side-bar'
@@ -41,76 +41,6 @@ const AppMain = () => {
 	)
 }
 AppMain.displayName = 'AppMain'
-
-const NavToolbar = ( {
-	actions,
-	appearance = 'light'
-} ) => {
-	const { isRoot, goToRoot } = useContext( Nav.Context )
-	const { label } = useContext( App.Context )
-	const labelStyle = {
-		padding: '2px 10px'
-	}
-	const iconWrapStyle = {
-		display: 'inline-flex',
-		transform: 'translateY(2px)',
-		padding: '0 4px 4px'
-	}
-
-	const style = {
-		pointerEvents: 'none',
-		display: 'flex',
-		flexDirection: 'row',
-		justifyContent: 'center',
-		alignItems: 'center',
-	}
-
-	const toolbarClasses = classname( {
-		'fl-asst-panel-toolbar': true,
-		'fl-asst-window-drag-handle': true,
-		'fl-asst-overlay-toolbar': true,
-		[`fl-asst-toolbar-appearance-${appearance}`]: appearance,
-	} )
-
-	const stopProp = e => e.stopPropagation()
-	const stopEvts = {
-		onMouseDown: stopProp,
-		onTouchStart: stopProp,
-	}
-
-	return (
-		<div className={ toolbarClasses }>
-			<span style={ style } { ...stopEvts }>
-				{ isRoot && <span style={ labelStyle }>{__( 'Assistant' )}</span> }
-
-				{ ! isRoot && <>
-					<Button
-						appearance="transparent"
-						onClick={ goToRoot }
-						style={ {
-							pointerEvents: 'auto',
-							textDecoration: 'underline',
-							padding: '0 10px',
-						} }
-					>{__( 'Assistant' )}</Button>
-					{ label && (
-						<>
-							<span style={ iconWrapStyle }>
-								<Icon.BreadcrumbArrow />
-							</span>
-							<span style={ labelStyle }>{label}</span>
-						</>
-					)}
-				</> }
-			</span>
-			{ actions && <span style={ {
-				marginLeft: 'auto',
-				display: 'flex',
-				alignItems: 'center'
-			} } { ...stopEvts }>{actions}</span> }
-		</div>
-	)
-}
 
 const AppContent = props => {
 	const { match } = props
