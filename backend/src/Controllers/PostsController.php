41f5c0283ffa7a447e8f80c4ff581596033e6a26
shahid <shahid.ajmeri@multidots.com>
2020-03-31 18:10:43 +0530

    Updated the unlink code.

--- backend/src/Controllers/PostsController.php
+++ backend/src/Controllers/PostsController.php
@@ -461,13 +461,8 @@ class PostsController extends ControllerAbstract {
 
 		/*Create Temporary file */
 
-		$file_url = WP_CONTENT_URL .'/'. $post->post_title . '_' . $post->ID . '.xml';
-		$file = WP_CONTENT_DIR .'/'. $post->post_title . '_' . $post->ID . '.xml';
-
-		if ( file_exists( $file ) ) {
-			unlink( $file );
-			$file = WP_CONTENT_DIR .'/'. $post->post_title . '_' . $post->ID . '.xml';
-		}
+		$file_url = WP_CONTENT_URL . '/' . $post->post_title . '_' . $post->ID . '.xml';
+		$file = WP_CONTENT_DIR . '/' . $post->post_title . '_' . $post->ID . '.xml';
 
 		$current = file_get_contents( $file );
 		// Append a new post to the file
@@ -545,7 +540,7 @@ class PostsController extends ControllerAbstract {
 
 		/*Remove Temporary file */
 
-		$file = WP_CONTENT_DIR .'/'. $post->post_title . '_' . $post->ID . '.xml';
+		$file = WP_CONTENT_DIR . '/' . $post->post_title . '_' . $post->ID . '.xml';
 
 		if ( file_exists( $file ) ) {
 			unlink( $file );
