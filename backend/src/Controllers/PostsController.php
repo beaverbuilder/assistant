6916d7cc68a32cb6b9b74a7e781e105f39be0c9d
shahid <shahid.ajmeri@multidots.com>
2020-04-10 12:51:05 +0530

    Fixes issue of BB layout and post meta data correction

--- backend/src/Controllers/PostsController.php
+++ backend/src/Controllers/PostsController.php
@@ -474,7 +474,7 @@ class PostsController extends ControllerAbstract {
 
 		$taxonomies_str = '';
 		foreach ( $terms as $term ) {
-			$taxonomies_str .= '<category domain="' . $term->taxonomy . '" nicename="' . $term->slug . '"><![CDATA[' . $term->name . ']]></category>';
+			$taxonomies_str .= '<category domain="' . $term->taxonomy . '" nicename="' . $term->slug . '">' . $this->wxr_cdata( $term->name ) . '</category>';
 		}
 
 		/* Creates comments string  */
@@ -487,14 +487,14 @@ class PostsController extends ControllerAbstract {
 
 			$comment_str .= '<wp:comment>
 			<wp:comment_id>' . $comment->comment_ID . '</wp:comment_id>
-			<wp:comment_author><![CDATA[' . $comment->comment_author . ']]></wp:comment_author>
-			<wp:comment_author_email><![CDATA[' . $comment->comment_author_email . ']]></wp:comment_author_email>
+			<wp:comment_author>' . $this->wxr_cdata( $comment->comment_author ) . '</wp:comment_author>
+			<wp:comment_author_email>' . $this->wxr_cdata( $comment->comment_author_email ) . '</wp:comment_author_email>
 			<wp:comment_author_url></wp:comment_author_url>
-			<wp:comment_author_IP><![CDATA[' . $comment->comment_author_ip . ']]></wp:comment_author_IP>
-			<wp:comment_date><![CDATA[' . $comment->comment_date . ']]></wp:comment_date>
-			<wp:comment_date_gmt><![CDATA[' . $comment->comment_date_gmt . ']]></wp:comment_date_gmt>
-			<wp:comment_content><![CDATA[' . $comment->comment_content . ']]></wp:comment_content>
-			<wp:comment_approved><![CDATA[' . $comment->comment_approved . ']]></wp:comment_approved>
+			<wp:comment_author_IP>' . $this->wxr_cdata( $comment->comment_author_ip ) . '</wp:comment_author_IP>
+			<wp:comment_date>' . $this->wxr_cdata( $comment->comment_date ) . '</wp:comment_date>
+			<wp:comment_date_gmt>' . $this->wxr_cdata( $comment->comment_date_gmt ) . '</wp:comment_date_gmt>
+			<wp:comment_content>' . $this->wxr_cdata( $comment->comment_content ) . '</wp:comment_content>
+			<wp:comment_approved>' . $this->wxr_cdata( $comment->comment_approved ) . '</wp:comment_approved>
 			<wp:comment_type><![CDATA[]]></wp:comment_type>
 			<wp:comment_parent>' . $comment->comment_parent . '</wp:comment_parent>
 			<wp:comment_user_id>' . $comment->user_id . '</wp:comment_user_id>
@@ -509,20 +509,46 @@ class PostsController extends ControllerAbstract {
 		foreach ( $meta_values as $key => $values ) {
 			foreach ( $values as $value ) {
 				$meta_str .= '<wp:postmeta>
-				<wp:meta_key><![CDATA[' . $key . ']]></wp:meta_key>
-				<wp:meta_value><![CDATA[ ' . $value . ']]></wp:meta_value>
+				<wp:meta_key>' . $this->wxr_cdata( $key ) . '</wp:meta_key>
+				<wp:meta_value>' . $this->wxr_cdata( $value ) . '</wp:meta_value>
 				</wp:postmeta>
 				';
 			}
 		}
+
+		/* Author data */
+
+		$author_display_name = $this->wxr_cdata( get_the_author_meta( 'display_name', $post->post_author ) );
+
+		/* Post content */
+
+		$post_content   = $this->wxr_cdata( $post->post_author );
+		$post_excerpt   = $this->wxr_cdata( $post->post_excerpt );
+		$post_date      = $this->wxr_cdata( $post->post_date );
+		$post_date_gmt  = $this->wxr_cdata( $post->post_date_gmt );
+		$comment_status = $this->wxr_cdata( $post->comment_status );
+		$ping_status    = $this->wxr_cdata( $post->ping_status );
+		$post_title     = $this->wxr_cdata( $post->post_title );
+		$post_status    = $this->wxr_cdata( $post->post_status );
+		$post_type      = $this->wxr_cdata( $post->post_type );
+
 		$export_data = $this->view->render_to_string(
 			'post-export',
 			[
-				'post'          => $post,
-				'taxonomy_data' => $taxonomies_str,
-				'comment_data'  => $comment_str,
-				'meta_data'     => $meta_str,
-
+				'post'           => $post,
+				'taxonomy_data'  => $taxonomies_str,
+				'comment_data'   => $comment_str,
+				'meta_data'      => $meta_str,
+				'author_name'    => $author_display_name,
+				'post_content'   => $post_content,
+				'post_excerpt'   => $post_excerpt,
+				'post_date'      => $post_date,
+				'post_date_gmt'  => $post_date_gmt,
+				'comment_status' => $comment_status,
+				'ping_status'    => $ping_status,
+				'post_title'     => $post_title,
+				'post_status'    => $post_status,
+				'post_type'      => $post_type,
 			]
 		);
 		file_put_contents( $file, $current . $export_data );
@@ -556,5 +582,16 @@ class PostsController extends ControllerAbstract {
 				]
 			);
 		}
+	}
+
+
+	function wxr_cdata( $str ) {
+		if ( ! seems_utf8( $str ) ) {
+			$str = utf8_encode( $str );
+		}
+		// $str = ent2ncr(esc_html($str));
+		$str = '<![CDATA[' . str_replace( ']]>', ']]]]><![CDATA[>', $str ) . ']]>';
+
+		return $str;
 	}
 }
