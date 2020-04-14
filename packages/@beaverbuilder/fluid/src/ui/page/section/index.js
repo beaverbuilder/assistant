6bf9d703d80e5c26c12674ae1e4ac2823a345da8
Brent Jett <brentjett@gmail.com>
2020-04-14 12:51:36 -0500

    Add `description` prop to `Page.Section`

--- packages/@beaverbuilder/fluid/src/ui/page/section/index.js
+++ packages/@beaverbuilder/fluid/src/ui/page/section/index.js
@@ -12,6 +12,7 @@ const Section = ( {
 	padX = true,
 	padY = true,
 	footer,
+	description,
 	...rest
 } ) => {
 
@@ -23,6 +24,7 @@ const Section = ( {
 	return (
 		<div className={ classes } { ...rest }>
 			{ label && <div className="fluid-section-title"><span className="fluid-section-title-text">{label}</span></div> }
+			{ description && <Layout.Row className="fluid-section-description">{description}</Layout.Row> }
 			<Layout.Box
 				className="fluid-section-content"
 				padX={ padX }
