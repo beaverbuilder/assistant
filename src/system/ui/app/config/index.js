debcc74e9a1114971ff74f3f6c354fbc39d842bd
Brent Jett <brentjett@gmail.com>
2020-05-01 14:37:32 -0500

    Remove animation test

--- src/system/ui/app/config/index.js
+++ src/system/ui/app/config/index.js
@@ -1,6 +1,5 @@
 import React, { memo } from 'react'
-import { Switch, Route, useHistory } from 'react-router-dom'
-import { AnimatePresence, motion, useReducedMotion } from 'framer-motion'
+import { Switch, Route } from 'react-router-dom'
 import { Page, Icon } from 'ui'
 
 const Config = ( {
@@ -9,22 +8,6 @@ const Config = ( {
 	...rest
 } ) => {
 	const { baseURL } = rest
-
-	/*
-	const history = useHistory()
-	const reduceMotion = useReducedMotion()
-	const getX = ( action, viewIs ) => {
-
-		if ( ! shouldAnimate || reduceMotion ) return 0
-
-		if ( 'POP' === action && 'exiting' === viewIs ) return '-100%'
-		if ( 'POP' === action && 'entering' === viewIs ) return '-100%'
-		if ( 'PUSH' === action && 'exiting' === viewIs ) return '100%'
-		if ( 'PUSH' === action && 'entering' === viewIs ) return '100%'
-		return 0
-	}
-	const transition = { duration: .15, type: "tween" }
-	*/
 
 	// Handle no pages in config
 	if ( ! pages || 0 > Object.keys( pages ).length ) {
@@ -51,60 +34,6 @@ const Config = ( {
 			<Route component={ Page.NotFound } />
 		</Switch>
 	)
-
-	/*
-	return (
-		<AnimatePresence initial={false}>
-			<motion.div
-				key={history.location.pathname}
-				initial={{
-					x: getX( history.action, 'entering' ),
-					position: 'absolute',
-					transition,
-					zIndex: 1
-				}}
-				animate={{
-					position: 'relative',
-					x: 0,
-					transition
-				}}
-				exit={{
-					position: 'absolute',
-					x: getX( history.action, 'exiting' ),
-					transition,
-					zIndex: 1
-				}}
-				style={{
-					flex: '1 1 auto',
-					maxHeight: '100%',
-					minHeight: 0,
-					display: 'flex',
-					flexDirection: 'column',
-					width: '100%',
-				}}
-			>
-				<Switch location={history.location} key={history.location.pathname}>
-					{ Object.entries( pages ).map( ( [ path, component ], i ) => {
-
-						const Component = memo(component)
-
-						return (
-							<Route
-								key={i}
-								exact={ 'default' === path }
-								path={ 'default' === path ? baseURL : baseURL + '/' + path }
-								render={ () => {
-		                            return <Component {...rest} />
-		                        } }
-							/>
-						)
-					})}
-					<Route component={ Page.NotFound } />
-				</Switch>
-			</motion.div>
-		</AnimatePresence>
-	)
-	*/
 }
 
 const NoPages = ( { label } ) => {
