e921ca63fb6ae151da77e9f30ad36bbacc4096bd
Brent Jett <brentjett@gmail.com>
2020-05-04 11:18:33 -0500

    Tweaks

--- src/apps/fl-home/app.js
+++ src/apps/fl-home/app.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { __ } from '@wordpress/i18n'
 import { App, Page, Button, Icon } from 'assistant/ui'
 import { CardPage } from './ui'
@@ -16,7 +16,6 @@ export default props => (
 )
 
 const CardsApp = () => {
-	const [ isEditing, setIsEditing ] = useState( false )
 
 	return (
 		<Page
@@ -25,22 +24,8 @@ const CardsApp = () => {
 			padY={ false }
 			toolbar={ false }
 		>
-			<div style={ {
-				display: 'flex',
-				flexDirection: 'row',
-				minHeight: 'var(--fluid-target-size)',
-				justifyContent: 'flex-end',
-				padding: 2
-			} }>
-				<Button
-					onClick={ () => setIsEditing( ! isEditing ) }
-				>
-					{ isEditing ? __( 'Done' ) : <Icon.Edit /> }
-				</Button>
-			</div>
 			<CardPage
 				page="home"
-				isEditing={ isEditing }
 			/>
 		</Page>
 	)
