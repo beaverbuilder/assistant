f90d7f2107e11bcbcbede0103e9aacd29a84f3fe
Justin Busa <justin@fastlinemedia.com>
2020-05-12 17:55:22 -0700

    Hooked up getAll, get, and create team endpoints methods to the cloud API.

--- src/system/utils/cloud/teams.js
+++ src/system/utils/cloud/teams.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import { __ } from '@wordpress/i18n'
 import { createSlug } from 'utils/url'
 import mockup from './mockup'
@@ -6,42 +7,56 @@ import http from './http'
 export default {
 	...mockup.teams,
 
-	// getAllTest: () => {
-	// 	return http.get( '/account/teams' )
-	// 		.then( ( response ) => {
-	//
-	// 			console.log( response )
-	// 			return response
-	// 		} )
-	// },
-	//
-	// create: ( data ) => {
-	// 	return http.post( '/account/teams/register', data )
-	// 		.then( ( response ) => {
-	//
-	// 			console.log( response )
-	// 			return response
-	// 		} )
-	// },
+	useAll: () => {
+		const [ teams, setTeams ] = useState( null )
+		useEffect( () => {
+			http.get( '/account/teams' ).then( response => {
+				setTeams( response.data )
+			} )
+		}, [] )
+		return [ teams, setTeams ]
+	},
+
+	useOne: ( id ) => {
+		const [ team, setTeam ] = useState( null )
+		useEffect( () => {
+			http.get( `/account/teams/${ id }` ).then( response => {
+				setTeam( response.data )
+			} )
+		}, [ id ] )
+		return [ team, setTeam ]
+	},
+
+	getAll: () => {
+		return http.get( '/account/teams' )
+	},
+
+	get: ( id ) => {
+		return http.get( `/account/teams/${ id }` )
+	},
 
 	create: ( data ) => {
-		return new Promise( ( resolve, reject ) => {
-			const teams = mockup.teams.getData().filter(
-				team => createSlug( team.name ) === createSlug( data.name )
-			)
-
-			if ( teams.length ) {
-				reject( {
-					errors: {
-						name: __( 'That name already exists.' )
-					}
-				} )
-				return
-			}
-
-			mockup.teams.create( data ).then( response => resolve( response ) )
-		} )
+		return http.post( '/account/teams/register', data )
 	},
+
+	// create: ( data ) => {
+	// 	return new Promise( ( resolve, reject ) => {
+	// 		const teams = mockup.teams.getData().filter(
+	// 			team => createSlug( team.name ) === createSlug( data.name )
+	// 		)
+	//
+	// 		if ( teams.length ) {
+	// 			reject( {
+	// 				errors: {
+	// 					name: __( 'That name already exists.' )
+	// 				}
+	// 			} )
+	// 			return
+	// 		}
+	//
+	// 		mockup.teams.create( data ).then( response => resolve( response ) )
+	// 	} )
+	// },
 
 	nameExists: ( name ) => {
 		return mockup.teams.getAll( { name } ).then( response => {
