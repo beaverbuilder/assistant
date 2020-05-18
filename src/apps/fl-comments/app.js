9574dd7578fcff9921073b9f5562c4a7da10ed97
Brent Jett <brentjett@gmail.com>
2020-05-18 13:47:24 -0500

    remove dummie post view from comments app

--- src/apps/fl-comments/app.js
+++ src/apps/fl-comments/app.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import { App } from 'assistant/ui'
-import { Main, Comment, Post } from './pages'
+import { Main, Comment } from './pages'
 
 export default props => (
 	<App.Config
 		pages={ {
 			default: Main,
 			'comment/:id': Comment,
-			'post/:id': Post,
 		} }
 		{ ...props }
 	/>
