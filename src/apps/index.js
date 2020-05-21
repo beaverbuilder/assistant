fc44f7fbe0a84c037471e6228bbc512c2e3d7e65
Brent Jett <brentjett@gmail.com>
2020-05-21 09:53:12 -0500

    clear out v1 stuff from comments and home apps

--- src/apps/index.js
+++ src/apps/index.js
@@ -2,14 +2,12 @@ __webpack_public_path__ = `${ FL_ASSISTANT_CONFIG.pluginURL }/build/`
 
 import './fl-home'
 import './fl-manage'
-
-//import './fl-search'
 import './fl-content'
 import './fl-media'
 import './fl-comments'
 import './fl-updates'
 import './fl-labels'
-import './fl-cloud'
-import './examples'
+//import './fl-cloud'
+//import './examples'
 
 import './integrations'
