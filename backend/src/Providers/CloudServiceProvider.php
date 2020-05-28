30b5278c64aae37339d5730a2a6676ba95de1f94
Justin Busa <justin@fastlinemedia.com>
2020-05-27 22:29:01 -0700

    Updated cloud URL.

--- backend/src/Providers/CloudServiceProvider.php
+++ backend/src/Providers/CloudServiceProvider.php
@@ -8,14 +8,9 @@ use FL\Assistant\System\Contracts\ServiceProviderAbstract;
 class CloudServiceProvider extends ServiceProviderAbstract {
 
 	public function bootstrap() {
-		$dev = defined( 'WP_DEBUG' ) && WP_DEBUG ? '-dev' : '';
 
-		// allow this to be overridden in wp-config.php for development
 		if ( ! defined( 'FL_ASSISTANT_CLOUD_URL' ) ) {
-			define( 'FL_ASSISTANT_CLOUD_URL', "https://dev-web-nyc3.wpassistant.cloud/api" );
-		}
-		if ( ! defined( 'FL_ASSISTANT_WP_API_URL' ) ) {
-			define( 'FL_ASSISTANT_WP_API_URL', "https://asst-wp$dev.network.fastlinemedia.com/wp-json" );
+			define( 'FL_ASSISTANT_CLOUD_URL', "" );
 		}
 	}
 }
