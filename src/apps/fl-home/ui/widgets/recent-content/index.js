b0c06d19eb72ebad7ac53c7cf1f36f6af106b8a0
Brent Jett <brentjett@gmail.com>
2020-06-01 20:58:31 -0500

    Remove count bar for now. Issues with list loading

--- src/apps/fl-home/ui/widgets/recent-content/index.js
+++ src/apps/fl-home/ui/widgets/recent-content/index.js
@@ -1,26 +1,24 @@
-import React, { memo } from 'react'
+import React, { memo, useState } from 'react'
 import { __ } from '@wordpress/i18n'
 import { List } from 'assistant/ui'
 import { Card } from 'home/ui'
-import CountBar from './count-bar'
 import './style.scss'
 
 const handle = 'fl-content'
 
 const RecentContentWidget = memo( () => {
+
 	return (
 		<Card
 			eyebrow={ __( 'Content' ) }
-			title={ __( 'Recent Updates' ) }
+			title={ __( 'Recent Posts' ) }
 			className="fl-asst-recent-content-card"
 		>
-			<CountBar />
 			<List.Posts
 				query={ {
 					post_type: 'post',
 					posts_per_page: 5
 				} }
-				paginate={ false }
 				getItemProps={ ( item, defaultProps ) => {
 					if ( item.id ) {
 						delete defaultProps.extras
