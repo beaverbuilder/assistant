70b0db231662684b88f6e68655ebb29c3e7564d1
Brent Jett <brentjett@gmail.com>
2020-06-08 12:31:27 -0500

    put library experiments app behind !__production__ flag

--- src/apps/fl-library-experiments/index.js
+++ src/apps/fl-library-experiments/index.js
@@ -7,7 +7,9 @@ const App = lazy( () => import(
 	/* webpackChunkName: "app-library-experiments" */ './app'
 ) )
 
-registerApp( 'fl-library-experiments', {
-	label: __( 'Library Experiments' ),
-	root: App,
-} )
+if ( ! __PRODUCTION__ ) {
+	registerApp( 'fl-library-experiments', {
+		label: __( 'Library Experiments' ),
+		root: App,
+	} )
+}
