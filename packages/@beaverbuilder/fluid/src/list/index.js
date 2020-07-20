39f650c4a028afc8dc297ef44de5e05998dbe9e3
Brent Jett <brentjett@gmail.com>
2020-07-19 20:53:30 -0500

    Continued work on lists

--- packages/@beaverbuilder/fluid/src/list/index.js
+++ packages/@beaverbuilder/fluid/src/list/index.js
@@ -1,10 +1,10 @@
 import Item from './item'
 import Section from './section'
-import Test from './test'
+import Iterator from './iterator'
 import './style.scss'
 
 export {
 	Section,
 	Item,
-	Test,
+	Iterator,
 }
