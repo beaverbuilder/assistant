2691b36daea2fcef92418cdcbe2863bd3522c32c
Brent Jett <brentjett@gmail.com>
2020-08-30 12:57:47 -0500

    Harden icon art against TwentyNineteen's fill rule

--- packages/@beaverbuilder/fluid/src/icon/person.js
+++ packages/@beaverbuilder/fluid/src/icon/person.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Person = ( { strokeWidth = 1.5, color = 'currentColor', ...rest } ) => {
 	return (
 		<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" { ...rest }>
-			<path d="M19 19C19 18.0475 18.748 16.9941 18.2119 16C17.0596 13.8632 14.5949 12 10.5 12H9.5" stroke={ color } strokeWidth={ strokeWidth } strokeLinecap="round"/>
-			<path d="M0.999999 19C0.999999 17.3599 1.74722 15.4209 3.40506 14" stroke={ color } strokeWidth={ strokeWidth } strokeLinecap="round"/>
-			<path d="M9.5 12C6.46243 12 4 9.53757 4 6.5C4 3.46243 6.46243 1 9.5 1C12.5376 1 15 3.46243 15 6.5C15 7.02001 14.9278 7.52317 14.793 8" stroke={ color } strokeWidth={ strokeWidth } strokeLinecap="round"/>
+			<path d="M19 19C19 18.0475 18.748 16.9941 18.2119 16C17.0596 13.8632 14.5949 12 10.5 12H9.5" stroke={ color } strokeWidth={ strokeWidth } strokeLinecap="round" fill="none" />
+			<path d="M0.999999 19C0.999999 17.3599 1.74722 15.4209 3.40506 14" stroke={ color } strokeWidth={ strokeWidth } strokeLinecap="round" fill="none" />
+			<path d="M9.5 12C6.46243 12 4 9.53757 4 6.5C4 3.46243 6.46243 1 9.5 1C12.5376 1 15 3.46243 15 6.5C15 7.02001 14.9278 7.52317 14.793 8" stroke={ color } strokeWidth={ strokeWidth } strokeLinecap="round" fill="none" />
 		</svg>
 	)
 }
--- src/apps/fl-media/icon.js
+++ src/apps/fl-media/icon.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const MediaIcon = () => (
 	<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<path d="M1 11V1H8.87175" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-		<path d="M8.00023 1H19.0002V13" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+		<path d="M1 11V1H8.87175" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" fill="none" />
+		<path d="M8.00023 1H19.0002V13" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" fill="none" />
 		<path d="M18.5819 20H1.82167C1.03258 20 0.554371 19.1288 0.978014 18.4631L7 9L11.375 13.7143L13.125 12.1429L19.2882 18.2921C19.9193 18.9217 19.4734 20 18.5819 20Z" fill="currentColor"/>
 		<circle cx="13" cy="7" r="2" fill="currentColor"/>
 	</svg>
