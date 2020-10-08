4ab055d2d0f91cf67b7a743790bf44cadbc74b46
Justin Busa <justin@fastlinemedia.com>
2020-10-08 15:03:44 -0700

    Only send full size images when saving posts to libraries, not derived sizes. Refactor post meta and term import logic. Refactor post sync method.

--- backend/src/Controllers/Cloud/Libraries/LibraryItemPostController.php
+++ backend/src/Controllers/Cloud/Libraries/LibraryItemPostController.php
@@ -96,21 +96,17 @@ class LibraryItemPostController extends ControllerAbstract {
 		);
 
 		$this->route(
-			'/posts/sync_from_library/(?P<id>\d+)/(?P<item_id>\d+)/(?P<override_type>\d+)',
+			'/posts/(?P<id>\d+)/sync_from_library/(?P<item_id>\d+)',
 			[
 				[
 					'methods'             => \WP_REST_Server::CREATABLE,
-					'callback'            => [ $this, 'override_lib_post' ],
+					'callback'            => [ $this, 'sync_from_library' ],
 					'args'                => [
 						'id'            => [
 							'required' => true,
 							'type'     => 'number',
 						],
 						'item_id'       => [
-							'required' => true,
-							'type'     => 'number',
-						],
-						'override_type' => [
 							'required' => true,
 							'type'     => 'number',
 						],
@@ -182,7 +178,7 @@ class LibraryItemPostController extends ControllerAbstract {
 		$id = $request->get_param( 'id' );
 		$library_id = $request->get_param( 'library_id' );
 		$post = get_post( $id );
-		$client = new \FL\Assistant\Clients\Cloud\CloudClient;
+		$client = new CloudClient;
 
 		return $client->libraries->create_item(
 			$library_id,
@@ -222,285 +218,41 @@ class LibraryItemPostController extends ControllerAbstract {
 	 * @return array
 	 */
 	public function import_from_library( $request ) {
-
-		global $wpdb;
-
 		$item_id = $request->get_param( 'item_id' );
 		$client = new CloudClient;
 		$response = $client->libraries->get_item( $item_id );
-
 		$post_data = $response->data->post;
-		$post_meta = $response->data->meta;
-		$post_terms  = $response->data->terms;
-		$current_user = wp_get_current_user();
 
-		$newpost_data = [
+		$new_post_id = wp_insert_post( [
 			'comment_status' => $post_data->comment_status,
 			'menu_order'     => $post_data->menu_order,
 			'ping_status' 	 => $post_data->ping_status,
-			'post_author'    => $current_user->ID,
+			'post_author'    => wp_get_current_user()->ID,
 			'post_content'   => $post_data->post_content,
-			'post_excerpt'	 => $post_data->post_excerpt,
+			'post_excerpt'	 => $post_data->post_excerpt ? $post_data->post_excerpt : '',
 			'post_mime_type' => $post_data->post_mime_type,
 			'post_name' 	 => $post_data->post_name,
 			'post_status'    => 'draft',
 			'post_title' 	 => $post_data->post_title,
 			'post_type' 	 => $post_data->post_type,
-		];
-
-		$new_post_id = wp_insert_post( $newpost_data );
+		] );
 
 		if ( is_wp_error( $new_post_id ) ) {
-
 			return rest_ensure_response(
 				[
 					'error' => true,
 				]
 			);
-
-		} else {
-
-			/*Import media */
-
-			$media = $response->media;
-			if ( $post_meta && count( $post_meta ) !== 0 ) {
-
-				foreach ( $post_meta as $key => $meta_value ) {
-
-					$meta_key = $key ? $key : '';
-
-					if ( '' !== $meta_key ) {
-						if ( count( $meta_value ) >= 1 ) {
-							foreach ( $meta_value as $value ) {
-								$value = $value ? addslashes( $value ) : '';
-								$wpdb->query( "INSERT INTO {$wpdb->postmeta} (post_id, meta_key, meta_value) values ({$new_post_id}, '{$meta_key}', '{$value}')" ); // phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
-							}
-						}
-					}
-				}
-			}
-
-			foreach ( $media->attachments as $key => $item ) {
-
-				// Check if already imported using $item->uuid
-				$row = $wpdb->get_row( "SELECT * FROM $wpdb->postmeta WHERE meta_value = '$item->uuid'" );
-
-				if ( $row ) {
-
-					// Already imported. Save the existing attachment ID.
-					$attachment_id = $row->post_id;
-					$attachment_url = wp_get_attachment_url( $attachment_id );
-
-					/*Get all media url from post content */
-
-					$replaced_post_content = get_post_field( 'post_content', $new_post_id );
-					$content_images = $this->get_post_content_image_paths( $replaced_post_content );
-					$content_images = $this->raw_media;
-
-					$update_post = [];
-					$update_post['ID'] = $new_post_id;
-
-					/*Loop all post content medias and replace it with new cloud imported URLS */
-					foreach ( $content_images[0] as $content_image ) {
-
-						if ( strpos( $content_image, $item->file_name ) ) {
-
-							$replaced_post_content = get_post_field( 'post_content', $new_post_id );
-							$update_post['post_content'] = str_replace( $content_image, $attachment_url, $replaced_post_content );
-							wp_update_post( $update_post );
-						}
-					}
-
-					/*Get all media url from post meta */
-					$post_meta_images = [];
-					if ( $post_meta && count( $post_meta ) !== 0 ) {
-
-						foreach ( $post_meta as $key => $meta_value ) {
-
-							$meta_key = $key ? $key : '';
-
-							if ( '' !== $meta_key ) {
-								if ( count( $meta_value ) >= 1 ) {
-									foreach ( $meta_value as $value ) {
-
-										$post_meta_images[] = $this->preg_grep_recursive( maybe_unserialize( $value ), true );
-
-									}
-								}
-							}
-						}
-					}
-
-					$post_meta_images = array_values( array_unique( $post_meta_images[0] ) );
-
-					$post_meta = get_post_meta( $new_post_id );
-					$item_file_name = $item->file_name;
-
-					/*Loop all post meta medias and replace it with new cloud imported URLS */
-					if ( ! empty( $post_meta_images ) ) {
-
-						foreach ( $post_meta_images as $postmeta_image ) {
-							$postmeta_image_url = $postmeta_image;
-
-							foreach ( $post_meta as $key => $meta_value ) {
-
-								$meta_key = $key ? $key : '';
-
-								if ( '' !== $meta_key ) {
-									if ( count( $meta_value ) >= 1 ) {
-										foreach ( $meta_value as $value ) {
-
-											if ( strpos( $postmeta_image_url, $item_file_name ) ) {
-
-												$get_value = $wpdb->get_results( "SELECT meta_value,meta_key from {$wpdb->postmeta} where post_id = '{$new_post_id}' AND meta_key = '{$meta_key}' AND meta_value LIKE '%{$postmeta_image}%'" ); // phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
-												if ( ! empty( $get_value ) ) {
-
-													$replaced_arr = $this->search_replace_array( maybe_unserialize( $get_value[0]->meta_value ), $postmeta_image, $attachment_url, $item->file_name );
-
-													update_post_meta( $new_post_id, $get_value[0]->meta_key, $replaced_arr );
-												}
-											}
-										}
-									}
-								}
-							}
-						}
-					}
-				} else {
-
-					// Not imported. Upload and save the new attachment ID.
-					$service = new MediaLibraryService();
-					$import_image_response = $service->import_image( $item->url, $item->file_name, $new_post_id );
-					$attachment_url = $import_image_response['url'];
-					update_post_meta( $import_image_response['id'], 'fl-asst-media-uuid', $item->uuid );
-					$item_file_name = $item->file_name;
-					$update_post = [];
-					$update_post['ID'] = $new_post_id;
-					$replaced_post_content = get_post_field( 'post_content', $new_post_id );
-					$content_images = $this->get_post_content_image_paths( $replaced_post_content );
-					$content_images = $this->raw_media;
-
-					foreach ( $content_images[0] as $content_image ) {
-						if ( strpos( $content_image, $item->file_name ) ) {
-
-							   $replaced_post_content = get_post_field( 'post_content', $new_post_id );
-							   $update_post['post_content'] = str_replace( $content_image, $attachment_url, $replaced_post_content ) . '<br><br>';
-							   wp_update_post( $update_post );
-
-						}
-					}
-
-					/*Get all media url from post meta */
-
-					if ( $post_meta && count( $post_meta ) !== 0 ) {
-
-						foreach ( $post_meta as $key => $meta_value ) {
-
-							$meta_key = $key ? $key : '';
-
-							if ( '' !== $meta_key ) {
-								if ( count( $meta_value ) >= 1 ) {
-									foreach ( $meta_value as $value ) {
-
-										   $post_meta_images[] = $this->preg_grep_recursive( maybe_unserialize( $value ), true );
-
-									}
-								}
-							}
-						}
-					}
-
-					$post_meta_images = array_values( array_unique( $post_meta_images[0] ) );
-					$post_meta = get_post_meta( $new_post_id );
-					/*Loop all post meta medias and replace it with new cloud imported URLS */
-					if ( ! empty( $post_meta_images ) ) {
-						foreach ( $post_meta_images as $postmeta_image ) {
-
-							if ( strpos( $postmeta_image, $item->file_name ) ) {
-								foreach ( $post_meta as $key => $meta_value ) {
-
-									$meta_key = $key ? $key : '';
-
-									if ( '' !== $meta_key ) {
-										if ( count( $meta_value ) >= 1 ) {
-											foreach ( $meta_value as $value ) {
-
-												if ( strpos( $postmeta_image_url, $item_file_name ) ) {
-
-													$get_value = $wpdb->get_results( "SELECT meta_value,meta_key from {$wpdb->postmeta} where post_id = '{$new_post_id}' AND meta_key = '{$meta_key}' AND meta_value LIKE '%{$postmeta_image}%'" ); // phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
-													if ( ! empty( $get_value ) ) {
-
-														$replaced_arr = $this->search_replace_array( maybe_unserialize( $get_value[0]->meta_value ), $postmeta_image, $attachment_url, $item->file_name );
-
-														update_post_meta( $new_post_id, $get_value[0]->meta_key, $replaced_arr );
-													}
-												}
-											}
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-
-			/*Importing feature image */
-			$thumbnail_data = $media->thumb;
-
-			// Check if already imported using $item->uuid
-			$row = $wpdb->get_row( "SELECT * FROM $wpdb->postmeta WHERE meta_value = '$thumbnail_data->uuid'" );
-
-			if ( $row ) {
-
-				// Already imported. Save the existing attachment ID.
-				$attachment_id = $row->post_id;
-				$attachment_url = wp_get_attachment_url( $attachment_id );
-				$success = set_post_thumbnail( $new_post_id, $attachment_id );
-
-			} else {
-
-				$service = new MediaLibraryService();
-				$import_image_response = $service->import_image( $thumbnail_data->url, $thumbnail_data->file_name, $new_post_id );
-				$attachment_url = $import_image_response['url'];
-				$attachment_id = $import_image_response['id'];
-				update_post_meta( $import_image_response['id'], 'fl-asst-media-uuid', $thumbnail_data->uuid );
-				$success = set_post_thumbnail( $new_post_id, $attachment_id );
-
-			}
-
-			/*Import post terms */
-
-			if ( is_array( $post_terms ) && count( $post_terms ) !== 0 ) {
-
-				foreach ( $post_terms as $category ) {
-					if ( taxonomy_exists( $category->taxonomy ) ) {
-						$term = term_exists( $category->slug, $category->taxonomy );
-						if ( 0 !== $term && null !== $term ) {
-							wp_set_post_terms( $new_post_id, [ $term['term_taxonomy_id'] ], $category->taxonomy, true );
-						} else {
-							$term = wp_insert_term(
-								$category->name,   // the term
-								$category->taxonomy, // the taxonomy
-								[
-									'description' => $category->description,
-									'slug'        => $category->slug,
-								]
-							);
-
-							wp_set_post_terms( $new_post_id, [ $term['term_taxonomy_id'] ], $category->taxonomy, true );
-						}
-					}
-				}
-			}
-
-			return rest_ensure_response(
-				$this->posts->transform(
-					get_post( $new_post_id )
-				)
-			);
 		}
 
+		$this->import_post_meta_from_library( $new_post_id, $response->data->meta );
+		$this->import_post_terms_from_library( $new_post_id, $response->data->terms );
+
+		return rest_ensure_response(
+			$this->posts->transform(
+				get_post( $new_post_id )
+			)
+		);
 	}
 
 	/**
@@ -510,114 +262,99 @@ class LibraryItemPostController extends ControllerAbstract {
 	 * @return array
 	 */
 	public function sync_from_library( $request ) {
-
-		global $wpdb;
 		$post_id = $request->get_param( 'id' );
 		$item_id = $request->get_param( 'item_id' );
-		$override_type = $request->get_param( 'override_type' );
-
 		$client = new CloudClient;
 		$response = $client->libraries->get_item( $item_id );
 
-		$post_data = $response->data->post;
-		$post_meta = $response->data->meta;
-		$post_terms  = $response->data->terms;
+		$updated = wp_update_post( [
+			'ID' => $post_id,
+			'post_content' => $response->data->post->post_content,
+		] );
 
-		$override_post = [];
-		$override_post['ID'] = $post_id;
-
-		if ( $post_meta && count( $post_meta ) !== 0 ) {
-
-			foreach ( $post_meta as $key => $meta_value ) {
-
-				$meta_key = $key ? $key : '';
-
-				if ( '' !== $meta_key ) {
-					if ( metadata_exists( 'post', $post_id, '_' . $meta_key ) ) {
-
-						update_metadata( 'post', $post_id, $meta_key, $meta_value );
-
-					} else {
-						if ( count( $meta_value ) >= 1 ) {
-							foreach ( $meta_value as $value ) {
-								$value = $value ? addslashes( $value ) : '';
-								$wpdb->query( "INSERT INTO {$wpdb->postmeta} (post_id, meta_key, meta_value) values ({$post_id}, '{$meta_key}', '{$value}')" ); // phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
-
-							}
-						}
-					}
-				}
-			}
-		}
-
-		if ( '2' === $override_type ) {
-
-			$override_post['post_content'] = $post_data->post_content;
-		} elseif ( '3' === $override_type ) {
-
-			if ( is_array( $post_terms ) && count( $post_terms ) !== 0 ) {
-
-				foreach ( $post_terms as $category ) {
-					if ( taxonomy_exists( $category->taxonomy ) ) {
-						$term = term_exists( $category->slug, $category->taxonomy );
-						if ( 0 !== $term && null !== $term ) {
-							wp_set_post_terms( $new_post_id, [ $term['term_taxonomy_id'] ], $category->taxonomy, true );
-						} else {
-							wp_insert_term(
-								$category->name,   // the term
-								$category->taxonomy, // the taxonomy
-								[
-									'description' => $category->description,
-									'slug'        => $category->slug,
-									'parent'      => $category->parent,
-								]
-							);
-
-							wp_set_post_terms( $new_post_id, [ $term['term_taxonomy_id'] ], $category->taxonomy, true );
-						}
-					}
-				}
-			}
-		} else {
-
-			$override_post['post_content'] = $post_data->post_content;
-			if ( is_array( $post_terms ) && count( $post_terms ) !== 0 ) {
-
-				foreach ( $post_terms as $category ) {
-					if ( taxonomy_exists( $category->taxonomy ) ) {
-						$term = term_exists( $category->slug, $category->taxonomy );
-						if ( 0 !== $term && null !== $term ) {
-							wp_set_post_terms( $new_post_id, [ $term['term_taxonomy_id'] ], $category->taxonomy, true );
-						} else {
-							wp_insert_term(
-								$category->name,   // the term
-								$category->taxonomy, // the taxonomy
-								[
-									'description' => $category->description,
-									'slug'        => $category->slug,
-									'parent'      => $category->parent,
-								]
-							);
-
-							wp_set_post_terms( $new_post_id, [ $term['term_taxonomy_id'] ], $category->taxonomy, true );
-						}
-					}
-				}
-			}
-		}
-
-		if ( is_wp_error( wp_update_post( $override_post ) ) ) {
+		if ( is_wp_error( $updated ) ) {
 			return rest_ensure_response(
 				[
 					'error' => true,
 				]
 			);
-		} else {
-			return rest_ensure_response(
-				$this->posts->transform(
-					get_post( $post_id )
-				)
-			);
+		}
+
+		$this->import_post_meta_from_library( $post_id, $response->data->meta );
+		$this->import_post_terms_from_library( $post_id, $response->data->terms );
+
+		return rest_ensure_response(
+			$this->posts->transform(
+				get_post( $post_id )
+			)
+		);
+	}
+
+	/**
+	 * Imports meta for a library post to the site.
+	 *
+	 * @param int $post_id
+	 * @param array $meta
+	 * @return void
+	 */
+	public function import_post_meta_from_library( $post_id, $meta ) {
+		global $wpdb;
+
+		if ( ! $meta || ! is_object( $meta ) ) {
+			return;
+		}
+
+		foreach ( $meta as $meta_key => $meta_value ) {
+			if ( metadata_exists( 'post', $post_id, $meta_key ) ) {
+				update_metadata( 'post', $post_id, $meta_key, $meta_value );
+			} else {
+				foreach ( $meta_value as $value ) {
+					$value = addslashes( $value );
+					// @codingStandardsIgnoreStart
+					$wpdb->query( "INSERT INTO {$wpdb->postmeta} (post_id, meta_key, meta_value) values ({$post_id}, '{$meta_key}', '{$value}')" );
+					// @codingStandardsIgnoreEnd
+				}
+			}
+		}
+	}
+
+	/**
+	 * Imports terms for a library post to the site.
+	 *
+	 * @param int $post_id
+	 * @param array $terms
+	 * @return void
+	 */
+	public function import_post_terms_from_library( $post_id, $terms ) {
+		if ( ! $terms || ! is_array( $terms ) || 0 === count( $terms ) ) {
+			return;
+		}
+
+		$taxonomies = get_taxonomies( '', 'objects' );
+
+		foreach ( $terms as $term ) {
+			if ( ! taxonomy_exists( $term->taxonomy ) ) {
+				continue;
+			}
+
+			$existing_term = term_exists( $term->slug, $term->taxonomy );
+			$is_hierarchical = !! $taxonomies[ $term->taxonomy ]->hierarchical;
+
+			if ( is_array( $existing_term ) ) {
+				$term_id = $is_hierarchical ? $existing_term['term_taxonomy_id'] : $term->name;
+				wp_set_post_terms( $post_id, [ $term_id ], $term->taxonomy, true );
+			} else {
+				$new_term = wp_insert_term(
+					$term->name,
+					$term->taxonomy,
+					[
+						'description' => $term->description,
+						'slug'        => $term->slug,
+					]
+				);
+				$term_id = $is_hierarchical ? $new_term['term_taxonomy_id'] : $term->name;
+				wp_set_post_terms( $post_id, [ $new_term['term_taxonomy_id'] ], $term->taxonomy, true );
+			}
 		}
 	}
 
@@ -710,6 +447,8 @@ class LibraryItemPostController extends ControllerAbstract {
 	}
 
 	/**
+	 * Converts a media library URL to the server path of the full size image.
+	 *
 	 * @param string $url
 	 * @return string
 	 */
@@ -718,6 +457,13 @@ class LibraryItemPostController extends ControllerAbstract {
 		$base_url = preg_replace( '/https?:\/\//', '', $upload_dir['baseurl'] );
 		$url = preg_replace( '/https?:\/\//', '', $url );
 		$path = null;
+
+		// Make sure we have the full size image url...
+		preg_match_all( '/(-\d+x\d+)\.(jpg|jpeg|png|gif)/', $url, $matches );
+
+		if ( isset( $matches[1] ) && ! empty( $matches[1] ) ) {
+			$url = str_replace( $matches[1], '', $url );
+		}
 
 		if ( stristr( $url, $base_url ) ) {
 			$path = $upload_dir['basedir'] . str_ireplace( $base_url, '', $url );
@@ -779,54 +525,6 @@ class LibraryItemPostController extends ControllerAbstract {
 		}
 
 		return $urls;
-	}
-
-	/**
-	 * @param array $input
-	 * @param bool $keys
-	 * @return array
-	 */
-	public function preg_grep_recursive( $input = [], $keys = false ) {
-
-		if ( is_object( $input ) || is_array( $input ) ) {
-
-			foreach ( $input as $key => $val ) {
-
-				if ( is_object( $val ) || is_array( $val ) ) {
-
-					$this->preg_grep_recursive( $val, $keys );
-				} else {
-
-					if ( preg_match_all( '#http?://[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|/)\.(jpg|jpeg|png))#', $val, $matches ) ) {
-
-						if ( isset( $matches[0][0] ) && '' !== $matches[0][0] ) {
-
-							$this->postmeta_images[] = $matches[0][0];
-						}
-
-						if ( isset( $matches[0][0] ) && '' !== $matches[0][0] && ! in_array( $matches[0][0], $this->output, true ) ) {
-
-							if ( $matches[0][0] ) {
-
-								array_push( $this->raw_media, $matches[0][0] );
-
-								$upload_media_path = $this->get_image_path_from_url( $matches[0][0] );
-
-								if ( $upload_media_path ) {
-									array_push( $this->output, $upload_media_path );
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-
-		if ( true === $keys ) {
-			return $this->postmeta_images;
-		}
-
-		return $this->output;
 	}
 
 	/**
