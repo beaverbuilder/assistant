bfd18e59794dc8fde3416caa32764e96bc510217
Brent Jett <brentjett@gmail.com>
2021-01-22 21:19:56 -0600

    Remove unused control components (Toggle and NextPrev)

--- src/system/ui/controls/index.js
+++ src/system/ui/controls/index.js
@@ -1,19 +1,8 @@
 import { Control } from '@beaverbuilder/forms'
-import { ToggleControl } from './toggle'
 import { URLControl } from './url'
-import { NextPrev } from './prev-next'
 import { CirclePicker } from './color-picker'
 
-Control.Toggle = ToggleControl
-Control.Toggle.displayName = 'Control.Toggle'
-
 Control.URL = URLControl
-Control.URL.displayName = 'Control.URL'
-
-Control.NextPrev = NextPrev
-Control.NextPrev.displayName = 'Control.NextPrev'
-
 Control.CirclePicker = CirclePicker
-Control.CirclePicker.displayName = 'Control.CirclePicker'
 
 export default Control
