9b7f55dd8cd47f6d72bc423dcd698825db21b98f
Brent Jett <brentjett@gmail.com>
2021-01-22 21:42:13 -0600

    remove unused App context - AppCore replaced this way back

--- src/system/ui/app/index.js
+++ src/system/ui/app/index.js
@@ -1,52 +1,9 @@
-import React, { createContext, useContext } from 'react'
-import { useLocation } from 'react-router-dom'
 import Config from './config'
 import List from './list'
 
-export const App = ( { children, ...rest } ) => {
-	return (
-		<div className="fl-asst-app-root" { ...rest }>
-			{children}
-		</div>
-	)
+const App = {
+	Config,
+	List
 }
 
-App.defaults = {
-	app: null,
-	handle: null,
-	isAppRoot: false
-}
-
-/**
- * Context
- */
-App.Context = createContext( App.defaults )
-
-/**
- * Provider
- */
-App.Provider = ( { children } ) => {
-	const location = useLocation()
-	const parts = location.pathname.split( '/' )
-	const name = parts[1]
-	const isAppRoot = 2 >= parts.length
-
-	const context = {
-		...App.defaults,
-		handle: name,
-		app: name,
-		isAppRoot
-	}
-
-	return (
-		<App.Context.Provider value={ context }>
-			{children}
-		</App.Context.Provider>
-	)
-}
-
-App.useApp = () => useContext( App.Context )
-
-App.Config = Config
-
-App.List = List
+export default App
