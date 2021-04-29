9d4df4227534260da54318470ed7890c2584f3a9
Brent Jett <brentjett@gmail.com>
2021-04-28 23:27:01 -0500

    Add initial app cards

--- src/apps/fl-home/ui/sections/apps/index.js
+++ src/apps/fl-home/ui/sections/apps/index.js
@@ -1,21 +1,62 @@
 import React from 'react'
+import { __ } from '@wordpress/i18n'
+import { Text } from 'assistant/ui'
+import { getSystemConfig } from 'assistant/data'
 import Section, { Swiper } from '../generic'
 import './style.scss'
 
 const AppsSection = () => {
+	const { pluginURL } = getSystemConfig()
+	const imgURL = `${pluginURL}/img/apps/fl-home/`
 	return (
 		<Section
 			className="fl-asst-apps-feature-section"
-			title="Get Started With Apps"
+			title={ __( 'Get Started With Apps' ) }
 			padContent={ false }
 		>
 			<Swiper>
-				<p className="fl-asst-swiper-item" style={ { background: 'var(--fl-brand-red)' } }>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Sed posuere consectetur est at lobortis. Nullam id dolor id nibh ultricies vehicula ut id elit. Etiam porta sem malesuada magna mollis euismod.</p>
-				<p className="fl-asst-swiper-item" style={ { background: 'var(--fl-brand-yellow)' } }>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Sed posuere consectetur est at lobortis. Nullam id dolor id nibh ultricies vehicula ut id elit. Etiam porta sem malesuada magna mollis euismod.</p>
-				<p className="fl-asst-swiper-item" style={ { background: 'var(--fl-brand-light-blue)' } }>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Sed posuere consectetur est at lobortis. Nullam id dolor id nibh ultricies vehicula ut id elit. Etiam porta sem malesuada magna mollis euismod.</p>
-				<p className="fl-asst-swiper-item" style={ { background: 'var(--fl-brand-purple)' } }>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Sed posuere consectetur est at lobortis. Nullam id dolor id nibh ultricies vehicula ut id elit. Etiam porta sem malesuada magna mollis euismod.Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+				<Card
+					headline={ <div>Find <strong>Content</strong> Fast</div> }
+					subtext={ __( 'The content app lets you find posts, pages and more with lightning speed.' ) }
+					style={ {
+						'--card-bg': 'var(--fl-brand-dark-blue)',
+						backgroundImage: `url(${imgURL}content-card.png)`,
+					} }
+				/>
+				<Card
+					headline={ <div><strong>Media</strong> At Your Fingertips</div> }
+					subtext={ __( 'The media app lets you quickly upload and find attachment info no matter where you are.' ) }
+					style={ {
+						'--card-bg': 'var(--fl-brand-red)',
+						backgroundImage: `url(${imgURL}media-card.png)`,
+					} }
+				/>
+				<Card
+					headline={ <div>Share <strong>Libraries</strong> across all your sites</div> }
+					style={ {
+						'--card-bg': 'var(--fl-brand-light-blue)',
+						'--card-color': '#125F77', /* little darker than normal dark blue */
+						backgroundImage: `url(${imgURL}libraries-card.png)`,
+					} }
+				/>
 			</Swiper>
 		</Section>
+	)
+}
+
+const Card = ( { headline, subtext, ...rest } ) => {
+	return (
+		<div
+			className="fl-asst-swiper-item"
+			{ ...rest }
+		>
+			<div style={ { padding: '0 20px', minHeight: 80 } }>
+				{ headline && <Text.Title>{headline}</Text.Title> }
+				{ subtext && <p>{subtext}</p> }
+			</div>
+			<div className="card-bottom-toolbar">
+			</div>
+		</div>
 	)
 }
 
