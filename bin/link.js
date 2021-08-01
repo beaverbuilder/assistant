9b108a8f1a8dfe554fad126ccc8c464c3ccfd2de
Justin Busa <justin@fastlinemedia.com>
2021-08-01 15:15:19 -0700

    Reworked link script

--- bin/link.js
+++ bin/link.js
@@ -1,23 +1,14 @@
 const path = require( 'path' )
+const fs = require( 'fs' )
 const cp = require( 'child_process' )
 const os = require( 'os' )
 
-const packages = [
-	'@beaverbuilder/app-core',
-	'@beaverbuilder/box',
-	'@beaverbuilder/cloud',
-	'@beaverbuilder/cloud-ui',
-	'@beaverbuilder/eslint-config',
-	'@beaverbuilder/fluid',
-	'@beaverbuilder/fluid-docs',
-	'@beaverbuilder/forms',
-	'@beaverbuilder/icons',
-	'@beaverbuilder/node-system',
-	'@beaverbuilder/webpack-config',
-]
+const packagePath = path.join( __dirname, '../node_modules/@beaverbuilder' )
+const packages = fs.readdirSync( packagePath, { withFileTypes: true } )
+	.filter( file => file.isDirectory() )
+	.map( file => `@beaverbuilder/${ file.name }` )
 
 const cmd = os.platform().startsWith( 'win' ) ? 'npm.cmd' : 'npm'
-
 cp.spawn( cmd, [ 'link', ...packages ], {
 	env: process.env,
 	cwd: path.join( __dirname, '../' ),
