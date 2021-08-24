769d6a5721295caf2de9b75d444b1278a797a9d7
Justin Busa <justin@fastlinemedia.com>
2021-08-24 14:24:28 -0700

    Updated cloud urls to production.

--- backend/src/Providers/CloudServiceProvider.php
+++ backend/src/Providers/CloudServiceProvider.php
@@ -10,10 +10,10 @@ class CloudServiceProvider extends ServiceProviderAbstract {
 	public function bootstrap() {
 
 		if ( ! defined( 'FL_ASSISTANT_CLOUD_URL' ) ) {
-			define( 'FL_ASSISTANT_CLOUD_URL', 'https://api.beta.assistant.pro' );
+			define( 'FL_ASSISTANT_CLOUD_URL', 'https://api.assistant.pro' );
 		}
 		if ( ! defined( 'FL_ASSISTANT_CLOUD_APP_URL' ) ) {
-			define( 'FL_ASSISTANT_CLOUD_APP_URL', 'https://beta.assistant.pro' );
+			define( 'FL_ASSISTANT_CLOUD_APP_URL', 'https://app.assistant.pro' );
 		}
 		if ( ! defined( 'FL_ASSISTANT_PUSHER_KEY' ) ) {
 			define( 'FL_ASSISTANT_PUSHER_KEY', 'd167736b3fc37d384caf' );
