56a646ed28b4556d484fddc8451b43e0af926638
Justin Busa <justin@fastlinemedia.com>
2021-10-26 13:26:20 -0700

    Don't connect cloud for WP users other than the one that originally made the connection.

--- src/apps/fl-cloud-connect/connect.js
+++ src/apps/fl-cloud-connect/connect.js
@@ -1,11 +1,12 @@
 import { getQueryArgs } from 'assistant/utils/url'
-import { getAppActions, getSystemActions } from 'assistant/data'
+import { getAppActions, getSystemActions, getSystemConfig } from 'assistant/data'
 import cloud from 'assistant/cloud'
 
 export default () => {
 	const LAST_TOKEN_KEY = 'fl-assistant-last-cloud-token'
 	const { setIsValidating } = getAppActions( 'fl-cloud-connect' )
 	const { setCloudUser } = getSystemActions()
+	const { currentUser } = getSystemConfig()
 	const { href } = window.location
 	const { token } = getQueryArgs( href )
 	const lastToken = localStorage.getItem( LAST_TOKEN_KEY )
@@ -16,7 +17,10 @@ export default () => {
 		cloud.session.setToken( token )
 		cloud.auth.refresh().then( response => {
 			cloud.session.create( response.token, response.user, true )
-			setCloudUser( response.user )
+			setCloudUser( {
+				...response.user,
+				wpId: currentUser.id
+			} )
 		} ).catch( () => {
 			cloud.session.destroy()
 		} ).finally( () => {
