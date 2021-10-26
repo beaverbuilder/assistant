66fe10b743042f1b891a70a19a90d74736bb6aa6
Brent Jett <brentjett@gmail.com>
2021-10-26 12:51:24 -0500

    reorder imports

--- src/apps/index.js
+++ src/apps/index.js
@@ -1,16 +1,16 @@
 __webpack_public_path__ = `${ FL_ASSISTANT_CONFIG.pluginURL }/build/`
 
 import './fl-home'
-import './fl-manage'
+import './libraries'
 import './fl-content'
 import './fl-media'
 import './fl-comments'
 import './fl-updates'
+import './fl-manage'
 import './fl-cloud-connect'
-import './libraries'
 
 // Product integrations
 import './integrations'
 
 // Hidden from production
-import './designsystem'
+//import './designsystem'
