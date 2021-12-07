515ee6d1267aed62c0a0daa352216a345a3c3460
Justin Busa <justin@fastlinemedia.com>
2021-12-07 10:26:01 -0800

    Fixed cloud logout on connected sites installed on the same domain.

--- src/apps/fl-cloud-connect/connect.js
+++ src/apps/fl-cloud-connect/connect.js
@@ -1,19 +1,20 @@
+import md5 from 'md5'
 import { getQueryArgs } from 'assistant/utils/url'
 import { getAppActions, getSystemActions, getSystemConfig } from 'assistant/data'
 import cloud from 'assistant/cloud'
 
 export default () => {
-	const LAST_TOKEN_KEY = 'fl-assistant-last-cloud-token'
+	const LAST_TOKEN_KEY = 'fl-assistant-last-cloud-token-'
 	const { setIsValidating } = getAppActions( 'fl-cloud-connect' )
 	const { setCloudUser } = getSystemActions()
-	const { currentUser } = getSystemConfig()
+	const { currentUser, homeUrl } = getSystemConfig()
 	const { href } = window.location
 	const { token } = getQueryArgs( href )
-	const lastToken = localStorage.getItem( LAST_TOKEN_KEY )
+	const lastToken = localStorage.getItem( LAST_TOKEN_KEY + md5( homeUrl ) )
 
 	if ( token && token !== lastToken ) {
 		setIsValidating( true )
-		localStorage.setItem( LAST_TOKEN_KEY, token )
+		localStorage.setItem( LAST_TOKEN_KEY + md5( homeUrl ), token )
 		cloud.session.setToken( token )
 		cloud.auth.refresh().then( response => {
 			cloud.session.create( response.token, response.user, true )
