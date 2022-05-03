9a730c0148259ae80ef7828195fbcf5ed23177f6
Brent Jett <brentjett@gmail.com>
2022-05-03 11:03:06 -0500

    setup tweak

--- src/apps/community/index.js
+++ src/apps/community/index.js
@@ -1,14 +1,12 @@
 import { lazy } from 'react'
 import { __ } from '@wordpress/i18n'
 import { registerApp } from 'assistant'
-import { getCommunityAppConfig } from '@beaverbuilder/cloud-ui'
 
 const App = lazy( () => import(
 	/* webpackChunkName: "app-discover" */ './app'
 ) )
 
 registerApp( 'discover', {
-	...getCommunityAppConfig(),
 	label: __( 'Discover' ),
 	root: App
 } )
