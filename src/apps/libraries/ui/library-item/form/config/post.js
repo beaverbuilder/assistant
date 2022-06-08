8471ebbfb2540adea0da0a84bdb0687cc0510f95
Justin Busa <justin@fastlinemedia.com>
2022-06-08 10:22:16 -0700

    Reworked library item import action to handle creating new posts and overriding existing.

--- src/apps/libraries/ui/library-item/form/config/post.js
+++ src/apps/libraries/ui/library-item/form/config/post.js
@@ -17,7 +17,7 @@ export const getActions = ( item, actions ) => {
 	} )
 
 	actions.unshift( {
-		component: <CreateButton item={ item } />
+		component: <ImportButton item={ item } />
 	} )
 
 	return actions
@@ -26,12 +26,12 @@ export const getActions = ( item, actions ) => {
 const PreviewButton = ( { item } ) => {
 	const { createNotice } = Libraries.ItemContext.use()
 	const [ previewing, setPreviewing ] = useState( false )
-	const api = getWpRest().libraries()
+	const librariesApi = getWpRest().libraries()
 
 	const previewPost = () => {
 		setPreviewing( true )
 
-		api.previewPost( item ).then( response => {
+		librariesApi.previewPost( item ).then( response => {
 			previewPostComplete( response )
 		} ).catch( () => {
 			previewPostComplete()
@@ -93,12 +93,14 @@ const UpdateButton = ( { item } ) => {
 				librariesApi.syncLibraryPost( id, item ).then( () => {
 					createNotice( {
 						status: 'success',
-						content: __( 'Library item updated!' )
+						content: __( 'Library item updated!' ),
+						shouldDismiss: false,
 					} )
 				} ).catch( () => {
 					createNotice( {
 						status: 'error',
-						content: __( 'Error updating library item.' )
+						content: __( 'Error updating library item.' ),
+						shouldDismiss: false,
 					} )
 				} ).finally( () => {
 					setUpdating( false )
@@ -144,16 +146,16 @@ const UpdateDialogContent = () => {
 	)
 }
 
-const CreateButton = ( { item } ) => {
-	const history = useHistory()
+const ImportButton = ( { item } ) => {
+	const { currentPageView, contentTypes } = getSystemConfig()
 	const { createNotice } = Libraries.ItemContext.use()
 	const [ action, setAction ] = useState( null )
 	const [ importingMedia, setImportingMedia ] = useState( false )
-	const api = getWpRest().libraries()
+	const librariesApi = getWpRest().libraries()
 	const importPostMedia = usePostMediaImport()
 
 	const createPost = () => {
-		api.importPost( item ).then( response => {
+		librariesApi.importPost( item ).then( response => {
 			setImportingMedia( true )
 			importPostMedia( response.data, item ).then( createPostComplete )
 		} ).catch( () => {
@@ -161,63 +163,99 @@ const CreateButton = ( { item } ) => {
 			setImportingMedia( false )
 			createNotice( {
 				status: 'error',
-				content: __( 'Error importing content.' )
+				content: __( 'Error importing library item.' )
 			} )
 		} )
 	}
 
 	const createPostComplete = ( post ) => {
-		if ( 'create' === action ) {
-			const { type } = item.data.builder
-			const { bbEditUrl, editUrl } = post
-
-			if ( 'beaver-builder' === type && bbEditUrl ) {
-				window.location.href = bbEditUrl
-			} else {
-				window.location.href = editUrl
-			}
-		} else {
-			importPostComplete( post )
-		}
+		const { editUrl, type } = post
+		const label = contentTypes[ type ] ? contentTypes[ type ].labels.singular : __( 'Content' )
 
 		setAction( null )
 		setImportingMedia( false )
-	}
 
-	const importPostComplete = ( post ) => {
 		if ( post.error ) {
 			createNotice( {
 				status: 'error',
-				content: __( 'Error importing content.' )
+				shouldDismiss: false,
+				content: __( 'Error importing library item.' )
 			} )
 		} else {
 			createNotice( {
 				status: 'success',
+				shouldDismiss: false,
 				content: (
 					<>
-						{ __( 'Content imported!' ) }
-						<a
-							style={ {
-								textDecoration: 'underline',
-								marginLeft: 'var(--fluid-sm-space)'
-							} }
-							onClick={ () => {
-								history.push( `/fl-content/post/${ post.id }`, {
-									item: post
-								} )
-							} }
-						>
-							{ __( 'View content.' ) }
-						</a>
+						{ __( 'Library item imported!' ) }
+						{ editUrl &&
+							<a
+								style={ {
+									textDecoration: 'underline',
+									marginLeft: 'var(--fluid-sm-space)'
+								} }
+								onClick={ () => {
+									window.location.href = editUrl
+								} }
+							>
+								{ sprintf( __( 'Edit %s.' ), label ) }
+							</a>
+						}
 					</>
 				)
 			} )
 		}
 	}
 
+	const replacePost = () => {
+		if ( ! currentPageView.isSingular ) {
+			alert( __( 'Replacing is currently not available. Navigate to the post or page you\'d like to replace and click the import button again.' ) )
+			setAction( null )
+			return
+		}
+
+		const message = sprintf( __( 'Do you really want to replace the current %s with this library item? This cannot be undone.' ), currentPageView.type.toLowerCase() )
+
+		if ( ! confirm( message ) ) {
+			setAction( null )
+			return
+		}
+
+		librariesApi.syncPost( currentPageView.id, item ).then( response => {
+			setImportingMedia( true )
+			importPostMedia( response.data, item ).then( () => {
+				replacePostComplete( response.data )
+			} )
+		} ).catch( () => {
+			replacePostComplete()
+		} )
+	}
+
+	const replacePostComplete = ( post ) => {
+		setAction( null )
+		setImportingMedia( false )
+
+		if ( ! post || post.error ) {
+			createNotice( {
+				status: 'error',
+				shouldDismiss: false,
+				content: sprintf( __( 'Error replacing %s.' ), currentPageView.type.toLowerCase() )
+			} )
+		} else {
+			createNotice( {
+				status: 'success',
+				shouldDismiss: false,
+				content: sprintf( __( '%s replaced!' ), currentPageView.type )
+			} )
+			window.location.reload()
+		}
+	}
+
 	useEffect( () => {
-		if ( action ) {
+		if ( 'create' === action ) {
 			createPost()
+		} else if ( 'replace' === action ) {
+			replacePost()
 		}
 	}, [ action ] )
 
@@ -229,100 +267,19 @@ const CreateButton = ( { item } ) => {
 				disabled={ action }
 			>
 				<option value=''>
-					{ ! importingMedia && ! action && __( 'Create New...' ) }
-					{ ! importingMedia && 'create' === action && __( 'Creating...' ) }
-					{ ! importingMedia && 'import' === action && __( 'Importing...' ) }
+					{ ! importingMedia && ! action && __( 'Import...' ) }
+					{ ! importingMedia && action && __( 'Importing...' ) }
 					{ importingMedia && __( 'Importing media...' ) }
 				</option>
-				<option value='create'>{ __( 'Create and Edit' ) }</option>
-				<option value='import'>{ __( 'Import' ) }</option>
+				<option value='create'>{ __( 'Create New' ) }</option>
+				<option value='replace'>
+					{
+						currentPageView.isSingular ?
+							sprintf( __( 'Replace Current %s' ), currentPageView.type ) :
+							__( 'Replace Current Page' )
+					}
+				</option>
 			</select>
 		</>
 	)
 }
-
-// const SyncSettings = ( { item } ) => {
-// 	const { currentPageView } = getSystemConfig()
-// 	const { intro, name, type, isSingular } = currentPageView
-//
-// 	if ( ! isSingular ) {
-// 		return (
-// 			<div>
-// 				{ __( 'Syncing is currently not available. To sync this library item, navigate to the post or page you\'d like to sync.' ) }
-// 			</div>
-// 		)
-// 	}
-//
-// 	return (
-// 		<div className='fl-asst-library-item-syncing'>
-// 			<div style={ { marginBottom: 'var(--fluid-med-space)' } }>
-// 				<strong>{ intro }:</strong> { name }
-// 			</div>
-// 			<div style={ { marginBottom: 'var(--fluid-lg-space)' } }>
-// 				{ sprintf(
-// 					__( 'Updating the library item will replace its content with the current %s. Updating the %s will replace its content with the library item.' ),
-// 					type.toLowerCase(),
-// 					type.toLowerCase()
-// 				) }
-// 			</div>
-// 			<Button.Group appearance='grid'>
-// 				<SyncLibraryItemButton item={ item } />
-// 				<SyncPostButton item={ item } />
-// 			</Button.Group>
-// 		</div>
-// 	)
-// }
-//
-// const SyncPostButton = ( { item } ) => {
-// 	const { currentPageView } = getSystemConfig()
-// 	const { id, type } = currentPageView
-// 	const [ syncing, setSyncing ] = useState( false )
-// 	const { createNotice } = Libraries.ItemContext.use()
-// 	const librariesApi = getWpRest().libraries()
-// 	const importPostMedia = usePostMediaImport()
-//
-// 	const syncPost = () => {
-// 		const message = sprintf( __( 'Do you really want to sync and replace the current %s with this library item? This cannot be undone.' ), type.toLowerCase() )
-//
-// 		if ( ! confirm( message ) ) {
-// 			return
-// 		}
-//
-// 		setSyncing( 'content' )
-//
-// 		librariesApi.syncPost( id, item ).then( response => {
-// 			setSyncing( 'media' )
-// 			importPostMedia( response.data, item ).then( () => {
-// 				syncPostComplete( response.data )
-// 			} )
-// 		} ).catch( () => {
-// 			syncPostComplete()
-// 		} )
-// 	}
-//
-// 	const syncPostComplete = ( post ) => {
-// 		setSyncing( false )
-//
-// 		if ( ! post || post.error ) {
-// 			createNotice( {
-// 				status: 'error',
-// 				content: __( 'Error syncing content.' )
-// 			} )
-// 		} else {
-// 			createNotice( {
-// 				status: 'success',
-// 				content: __( 'Content replaced!' )
-// 			} )
-// 			window.location.reload()
-// 		}
-// 	}
-//
-// 	return (
-// 		<Button onClick={ syncPost } disabled={ syncing }>
-// 			<Icon.Download />
-// 			{ ! syncing && sprintf( __( 'Update %s' ), type ) }
-// 			{ 'content' === syncing && __( 'Syncing content...' ) }
-// 			{ 'media' === syncing && __( 'Syncing media...' ) }
-// 		</Button>
-// 	)
-// }
