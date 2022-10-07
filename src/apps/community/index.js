01593feaa09fe4cd72138128c7230ad4fe508f7d
rahulwalunje <rahulwalunje.09@gmail.com>
2022-10-07 17:35:45 +0530

    Hide community app if not admin

--- src/apps/community/index.js
+++ src/apps/community/index.js
@@ -1,12 +1,17 @@
 import { lazy } from 'react'
 import { __ } from '@wordpress/i18n'
 import { registerApp } from 'assistant'
+import cloud from 'assistant/cloud'
 
+const user = cloud.session.getUser()
+const isAdmin = null === user ? false : user?.is_admin
+const isConnected = cloud.auth.isConnected()
 const App = lazy( () => import(
 	/* webpackChunkName: "app-community" */ './app'
 ) )
 
 registerApp( 'community', {
 	label: __( 'Community' ),
-	root: App
+	root: App,
+	enabled: isAdmin && isConnected
 } )
